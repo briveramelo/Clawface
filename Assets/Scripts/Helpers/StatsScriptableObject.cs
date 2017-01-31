5b664b0af67050c99b8aa8373394e7e2d8598da7
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2017-01-31 12:27:05 -0700

    Layers and CharacterType enums added. Non-working stats class in place

---
+++ Assets/Scripts/Helpers/StatsScriptableObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class StatsObject : ScriptableObject
+{
+    public float attack, defense, health, moveSpeed, miniMapRange, rangedAccuracy;
+
+    public void Modify(StatType statType, float mod) {
+        switch (statType) {
+            case StatType.Attack:
+                attack *= mod;
+                break;
+            case StatType.Defense:
+                defense *= mod;
+                break;
+            case StatType.Health:
+                health *= mod;
+                break;
+            case StatType.MiniMapRange:
+                moveSpeed *= mod;
+                break;
+            case StatType.MoveSpeed:
+                miniMapRange *= mod;
+                break;
+            case StatType.RangedAccuracy:
+                rangedAccuracy *= mod;
+                break;
+        }
+    }
+}
