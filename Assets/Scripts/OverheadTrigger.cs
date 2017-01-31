bef7b953740886701be20aa9794596b856f4f53c
MegaSonic <adamkay90@gmail.com>
2017-01-31 12:19:21 -0700

    Adding some camera transition scripts I'll eventually need

---
+++ Assets/Scripts/OverheadTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverheadTrigger : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SendMessageUpwards("OverheadTriggerEnter");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SendMessageUpwards("OverheadTriggerStay");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SendMessageUpwards("OverheadTriggerExit");
+        }
+    }
+}
+
---
+++ Assets/Scripts/SidescrollTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SidescrollTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SendMessageUpwards("SidescrollTriggerEnter");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SendMessageUpwards("SidescrollTriggerStay");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SendMessageUpwards("SidescrollTriggerExit");
+        }
+    }
+}
