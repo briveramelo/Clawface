cb5a2fbcde858aecac0303dbd72bbaed51a1d8b3
Chuan-Chin Lai <danny6430@hotmail.com>
2017-02-02 12:29:49 -0700

    add Elevator

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,58 @@
+﻿//Elevator created by Lai
+//Use keyboard A to tirgger
+//Player has to have Rigidbody with Gravity
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+
+    public float moveTime = 10.0f;
+    public float moveSpeed = 1.0f;
+
+    private bool isMoving = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A) && isMoving == false) 
+        {
+            StartCoroutine(moveElevator());
+        }
+    }
+
+
+    IEnumerator moveElevator()
+    {
+        float time = moveTime;
+        isMoving = true;
+
+        while(time >= 0.0f)
+        {
+            transform.Translate(moveSpeed * Vector3.up * Time.fixedDeltaTime);
+            time -= Time.fixedDeltaTime;
+            yield return 0;
+        }
+
+        yield return new WaitForSeconds(2);
+        time = moveTime;
+
+        while (time >= 0.0f)
+        {
+            transform.Translate(moveSpeed * Vector3.down * Time.fixedDeltaTime);
+            time -= Time.fixedDeltaTime;
+            yield return 0;
+        }
+
+        time = moveTime;
+        isMoving = false;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(speed * Vector3.forward * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(speed * Vector3.back * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(speed * Vector3.left * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Vector3.right * Time.deltaTime);
+        }
+    }
+}
