dd379f5f9b0f43a540bed9d013f5159481b11fc5
Chuan-Chin Lai <danny6430@hotmail.com>
2017-02-02 14:11:43 -0700

    fixed elevator

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -8,51 +8,67 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
+    enum State
+    {
+        Up = 0,
+        Moving = 1, 
+        Down = 2,
+    }
 
-    public float moveTime = 10.0f;
     public float moveSpeed = 1.0f;
 
-    private bool isMoving = false;
+    private State m_state = State.Down;
+    private Vector3 initPosition = Vector3.zero;
+    private Vector3 maxPosition = Vector3.zero;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-		
+        initPosition = transform.position;
+        maxPosition = transform.GetChild(0).transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.A) && isMoving == false) 
+        if (Input.GetKey(KeyCode.A) && m_state == State.Down) 
         {
-            StartCoroutine(moveElevator());
+            StartCoroutine(moveUp());
+        }
+
+        if (Input.GetKey(KeyCode.S) && m_state == State.Up)
+        {
+            StartCoroutine(moveDown());
         }
     }
 
 
-    IEnumerator moveElevator()
+    IEnumerator moveUp()
     {
-        float time = moveTime;
-        isMoving = true;
+        m_state = State.Moving;
 
-        while(time >= 0.0f)
+        while (transform.position.y <= maxPosition.y)
         {
             transform.Translate(moveSpeed * Vector3.up * Time.fixedDeltaTime);
-            time -= Time.fixedDeltaTime;
             yield return 0;
         }
 
         yield return new WaitForSeconds(2);
-        time = moveTime;
+        m_state = State.Up;
+    }
 
-        while (time >= 0.0f)
+
+    IEnumerator moveDown()
+    {
+        m_state = State.Moving;
+
+        while (transform.position.y >= initPosition.y)
         {
             transform.Translate(moveSpeed * Vector3.down * Time.fixedDeltaTime);
-            time -= Time.fixedDeltaTime;
             yield return 0;
         }
 
-        time = moveTime;
-        isMoving = false;
+        yield return new WaitForSeconds(2);
+        m_state = State.Down;
     }
 }
