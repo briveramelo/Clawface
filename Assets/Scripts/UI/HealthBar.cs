69d108e59a32e3b3f72463ac3e8174ef99d59973
Cornelia Xaos <cornelia.xaos@gmail.com>
2017-02-04 18:09:24 -0700

    Health Bar Progress

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿/**
+ *  @author Cornelia Schultz
+ */
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class HealthBar : MonoBehaviour {
+
+    //// Unity Inspector Fields
+    [SerializeField]
+    private Transform bar;
+
+    //// HealthBar Public Interface
+    public void SetHealth(float health)
+    {
+        Assert.IsTrue(health >= 0.0F && health <= 1.0F);
+        bar.localScale = new Vector3(health, 1.0F, 1.0F);
+    }
+}
