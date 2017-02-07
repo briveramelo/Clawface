23948e4a4d7997238e189e6dc5f8a5461c026beb
Cornelia Xaos <cornelia.xaos@gmail.com>
2017-02-07 10:14:13 -0700

    Finalized Health Bar for project.

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -9,12 +9,13 @@ public class HealthBar : MonoBehaviour {
 
     //// Unity Inspector Fields
     [SerializeField]
-    private Transform bar;
+    private Transform mask, bar;
 
     //// HealthBar Public Interface
     public void SetHealth(float health)
     {
         Assert.IsTrue(health >= 0.0F && health <= 1.0F);
-        bar.localScale = new Vector3(health, 1.0F, 1.0F);
+        mask.localScale = new Vector3(health, 1.0F, 1.0F);
+        bar.localScale = new Vector3(1 / health, 1.0F, 1.0F);
     }
 }
