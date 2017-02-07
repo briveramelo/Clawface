899adb4769196c835446c7b129ef091a74d0c8c3
garink <garinrk@gmail.com>
2017-02-07 12:53:47 -0700

    SpeedPad uses the IMovable interface versus calling player movement directly

--- Assets/Scripts/Environment/SpeedPad.cs
+++ Assets/Scripts/Environment/SpeedPad.cs
@@ -5,64 +5,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpeedPad : MonoBehaviour {
-    PlayerMovement pm;
 
-    //[SerializeField]
-    //float cooldown = 2.0f;
-
-    //float cooldownTimer;
-
+    IMovable im;
     Vector3 direction;
     Vector3 pushVector;
 
-    [SerializeField]
-    float force = 5.0f;
+    [SerializeField, Range(5,15)]
+    float force;
 
     bool activated = false;
     void OnTriggerStay(Collider col)
     {
+   
+        im = col.gameObject.GetComponent<IMovable>();
 
-            pm = col.gameObject.GetComponent<PlayerMovement>();
-            if (pm)
-            {
-                PushPlayer(pushVector,pm);
-
-            }
-            
+        if (im != null)
+        {
+            im.AddExternalForce(pushVector);
+        }
         
     }
 
-    //private void FixedUpdate()
-    //{
-    //    if (activated)
-    //    {
-    //        cooldownTimer -= Time.fixedDeltaTime;
-    //        if(cooldownTimer <= 0f)
-    //        {
-    //            activated = false;
-    //            cooldownTimer = cooldown;
-    //        }
-    //    }
-    //}
+    
 
     private void Awake()
     {
         direction = gameObject.transform.forward;
         pushVector = direction;
         pushVector.z += force;
-        //cooldownTimer = cooldown;
     }
 
-    void PushPlayer(Vector3 i_force, PlayerMovement pm)
-    {
-        //if (!activated)
-        //{
-        //    activated = true;
-
-        //    pm = null;
-        //}
-        pm.AddExternalForce(pushVector);
-    }
 
     
 }
