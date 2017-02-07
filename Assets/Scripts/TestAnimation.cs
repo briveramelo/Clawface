9019f9a8396001e824996204a9b4f9eaf6bf4c38
Bharat Gudihal <bharat.gudi@gmail.com>
2017-02-07 11:54:26 -0700

    testing animations

---
+++ Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimation : MonoBehaviour {
+
+    [SerializeField]
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("StartWalking", !animator.GetBool("StartWalking"));
+        }
+	}
+}
