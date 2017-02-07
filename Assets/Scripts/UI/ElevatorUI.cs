e68275422e8bc410cbe2664dd3de35fcb3c78411
Cornelia Xaos <cornelia.xaos@gmail.com>
2017-02-07 10:38:56 -0700

    Fixed some things with the Elevator UI

--- Assets/Scripts/UI/ElevatorUI.cs
+++ Assets/Scripts/UI/ElevatorUI.cs
@@ -6,24 +6,29 @@ public class ElevatorUI : MonoBehaviour {
     // An enum to make things a little easier for now
     public enum Floor
     {
-        ONE,
-        TWO,
-        THREE
+        ARMORY,
+        TRIALS,
+        ARENA
     }
 
     //// Unity Inspector Fields
     [SerializeField]
-    private Image floor3, floor2, floor1;
+    private Image arena, trials, armory;
 
     //// Constants
     private static Color UNSELECTED = new Color(0x32 / 255.0F, 0xD2 / 255.0F, 0xBE / 255.0F);
     private static Color SELECTED = new Color(0x32 / 255.0F, 0x83 / 255.0F, 0xD2 / 255.0F);
 
     //// Elevator UI Public Interface
+    public void SetVisible(bool visible)
+    {
+        gameObject.SetActive(visible);
+    }
+
     public void SelectFloor(Floor floor)
     {
-        floor3.color = floor == Floor.THREE ? SELECTED : UNSELECTED;
-        floor2.color = floor == Floor.TWO ? SELECTED : UNSELECTED;
-        floor1.color = floor == Floor.ONE ? SELECTED : UNSELECTED;
+        arena.color = floor == Floor.ARENA ? SELECTED : UNSELECTED;
+        trials.color = floor == Floor.TRIALS ? SELECTED : UNSELECTED;
+        armory.color = floor == Floor.ARMORY ? SELECTED : UNSELECTED;
     }
 }
