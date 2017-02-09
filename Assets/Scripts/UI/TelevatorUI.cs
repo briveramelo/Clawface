9fcba97f4c713f9517fcd76c7b5ac648b6b714a1
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2017-02-09 02:38:46 -0700

    Set up the mainscene brandon to work with the televator ui

--- Assets/Scripts/UI/TelevatorUI.cs
+++ Assets/Scripts/UI/TelevatorUI.cs
@@ -13,11 +13,11 @@ public class TelevatorUI : MonoBehaviour {
 
     //// Unity Inspector Fields
     [SerializeField]
-    private Image arena, trials, armory;
+    private Image arena, armory, trials;
 
     //// Constants
-    private static Color UNSELECTED = new Color(0x32 / 255.0F, 0xD2 / 255.0F, 0xBE / 255.0F);
-    private static Color SELECTED = new Color(0x32 / 255.0F, 0x83 / 255.0F, 0xD2 / 255.0F);
+    private static Color SELECTED = new Color(0x32 / 255.0F, 0xD2 / 255.0F, 0xBE / 255.0F);
+    private static Color UNSELECTED = new Color(0x32 / 255.0F, 0x83 / 255.0F, 0xD2 / 255.0F);
 
     //// Elevator UI Public Interface
     public void SetVisible(bool visible)
