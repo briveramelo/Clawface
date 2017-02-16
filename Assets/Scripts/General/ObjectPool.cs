65431d548f5776a2bb7cc5169588bc226311f254
garink <garinrk@gmail.com>
2017-02-16 16:03:04 -0700

    using object pool for blood

--- Assets/Scripts/General/ObjectPool.cs
+++ Assets/Scripts/General/ObjectPool.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : Singleton<ObjectPool> {
+public class ObjectPool : Singleton<ObjectPool>
+{
 
     #region Unity Inspector Fields
-    [SerializeField] private Pool 
+    [SerializeField]
+    private Pool
         stunMinePool,
         blasterBulletPool,
         blasterImpactEffectPool,
         mallCopPool,
         targetExplosionEffectPool,
-        stunMineExplosionEffect;
+        stunMineExplosionEffect,
+        bloodSplatterPool;
     //Add new pools here
     #endregion
 
@@ -20,7 +23,8 @@ public class ObjectPool : Singleton<ObjectPool> {
     #endregion
 
     #region Unity LifeCycle
-    private void Start () {
+    private void Start()
+    {
         stunMinePool.Initialize(transform);
         blasterBulletPool.Initialize(transform);
         blasterImpactEffectPool.Initialize(transform);
@@ -37,15 +41,17 @@ public class ObjectPool : Singleton<ObjectPool> {
             { PoolObjectType.MallCop, mallCopPool },
             { PoolObjectType.TargetExplosionEffect, targetExplosionEffectPool },
             { PoolObjectType.MineExplosionEffect, stunMineExplosionEffect },
-            
+            { PoolObjectType.BloodDecal, bloodSplatterPool}
             //Add new pools here
         };
     }
     #endregion
 
     #region Public Methods
-    public GameObject GetObject(PoolObjectType poolObject) {
-        if (pools.ContainsKey(poolObject)) {
+    public GameObject GetObject(PoolObjectType poolObject)
+    {
+        if (pools.ContainsKey(poolObject))
+        {
             return pools[poolObject].GetObject();
         }
         string debugMessage = "No object found. Create a pool for this object";
@@ -60,15 +66,18 @@ public class ObjectPool : Singleton<ObjectPool> {
     {
         public GameObject prefab;
         public int size;
-        [HideInInspector] public List<GameObject> objects;
+        [HideInInspector]
+        public List<GameObject> objects;
 
-        public Pool(ref GameObject prefab, int size) {
+        public Pool(ref GameObject prefab, int size)
+        {
             this.objects = new List<GameObject>();
             this.prefab = prefab;
             this.size = size;
         }
 
-        public void Initialize(Transform grandparent) {
+        public void Initialize(Transform grandparent)
+        {
             GameObject parent = new GameObject(prefab.name);
             parent.transform.SetParent(grandparent);
             for (int index = 0; index < size; index++)
@@ -82,7 +91,8 @@ public class ObjectPool : Singleton<ObjectPool> {
         public GameObject GetObject()
         {
             GameObject objToReturn = objects.Find(obj => !obj.activeSelf);
-            if (objToReturn==null) {
+            if (objToReturn == null)
+            {
                 string debugMessage = "No more" + prefab.name + " objects found! Make your pool bigger than " + size;
                 Debug.LogFormat("<color=#ffff00>" + debugMessage + "</color>");
             }
