fb22f873dba4fb6c3eecc82dfd8fa680fe854e10
MegaSonic <adamkay90@gmail.com>
2017-02-16 09:20:52 -0700

    Changed TimeManager to HitstopManager

---
+++ Assets/Scripts/General/HitstopManager.cs
@@ -0,0 +1,50 @@
+﻿// Adam Kay
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitstopManager : Singleton<HitstopManager> {
+
+    [SerializeField]
+    private float remainingHitstop;
+
+    [SerializeField]
+    private bool isInHitstop;
+
+    private float hitstopTimer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (remainingHitstop > 0f)
+        {
+            remainingHitstop -= Time.deltaTime;
+
+            if (remainingHitstop <= 0f)
+            {
+                StopHitstop();
+            }
+        }
+	}
+
+    public bool IsInHitstop()
+    {
+        return isInHitstop;
+    }
+
+    public void StartHitstop(float time)
+    {
+        remainingHitstop = time;
+        isInHitstop = true;
+    }
+
+    private void StopHitstop()
+    {
+        isInHitstop = false;
+    }
+}
