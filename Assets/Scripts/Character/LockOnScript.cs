1be1eb5cc6af955dcc595a0b33d2cfc9e21f79e6
Bharat Gudihal <bharat.gudi@gmail.com>
2017-02-16 09:18:41 -0700

    got the sphere cast working

---
+++ Assets/Scripts/Character/LockOnScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOnScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        if(Input.GetAxis(Strings.RIGHTTRIGGER) < -0.5f){
+            AcquireTarget();
+        }
+	}
+
+    void AcquireTarget()
+    {
+        RaycastHit[] hits;
+        Ray sphereRay = new Ray(transform.position, transform.forward);
+        LayerMask mask = LayerMask.GetMask("Enemy");
+        hits = Physics.SphereCastAll(sphereRay, 5f, 0f, mask);        
+        foreach(RaycastHit hit in hits)
+        {
+            if (hit.transform.gameObject.tag == Strings.ENEMY)
+            {
+                print(hit.transform.gameObject.name);
+            }
+        }
+    }
+}
