5cf47243578c7731f1fb5eb2de4bec61396b9aca
garink <garinrk@gmail.com>
2017-03-26 18:29:27 -0600

    Made re-placed health bar billboard to the camera. Prefab'd the PlayerUI.

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -7,6 +7,10 @@ using UnityEngine.Assertions;
 
 public class HealthBar : Singleton<HealthBar> {
 
+    private Camera m_Camera;
+
+
+
     protected HealthBar() { }
 
     //// Unity Inspector Fields
@@ -16,9 +20,22 @@ public class HealthBar : Singleton<HealthBar> {
     //// HealthBar Public Interface
     public void SetHealth(float health)
     {
-        Debug.Log("Health is set: " + health);
         Assert.IsTrue(health >= 0.0F && health <= 1.0F);
         mask.localScale = new Vector3(health, 1.0F, 1.0F);
         bar.localScale = new Vector3(health == 0 ? 0 : 1 / health, 1.0F, 1.0F);
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        m_Camera = Camera.main;
+ 
+    }
+
+    private void Update()
+    {
+        //make health bar billboard to main camera.
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+                    m_Camera.transform.rotation * Vector3.up);
     }
 }
