f749a386a82b17ca8e6d5e40e488cc8a2f7f1c77
Aaron Desin <aaron@aarondesin.com>
2017-03-28 15:00:29 -0600

    Blood material/masks, editor icons

--- Assets/Scripts/Audio/AudioChannel.cs
+++ Assets/Scripts/Audio/AudioChannel.cs
@@ -4,179 +4,189 @@ using System.Collections.Generic;
 using UnityEngine;
 using ModMan;
 
-/// <summary>
-/// Audio class for an individual channel attached to an AudioGroup.
-/// </summary>
-[System.Serializable]
-[ExecuteInEditMode]
-public class AudioChannel : MonoBehaviour {
-
-    #region Vars
+namespace Turing.Audio {
 
     /// <summary>
-    /// AudioSource to play from (should be attached to gameObject).
+    /// Audio class for an individual channel attached to an AudioGroup.
     /// </summary>
-    AudioSource _audioSource;
+    [System.Serializable]
+    [ExecuteInEditMode]
+    public class AudioChannel : MonoBehaviour {
 
-    /// <summary>
-    /// AudioElement that uses this AudioElementChannel.
-    /// </summary>
-    AudioGroup _parent;
+        #region Vars
 
-    /// <summary>
-    /// Will this AudioChannel loop?
-    /// </summary>
-    bool _loop = false;
+        /// <summary>
+        /// AudioSource to play from (should be attached to gameObject).
+        /// </summary>
+        AudioSource _audioSource;
 
-    /// <summary>
-    /// Is this AudioChannel playing?
-    /// </summary>
-    bool _playing = false;
+        /// <summary>
+        /// AudioElement that uses this AudioElementChannel.
+        /// </summary>
+        AudioGroup _parent;
 
-    /// <summary>
-    /// List of AudioClips to play in this channel.
-    /// </summary>
-    [SerializeField]
-    [Tooltip("List of AudioClips to play in this channel.")]
-    List<AudioClip> _clips = new List<AudioClip>();
+        /// <summary>
+        /// Will this AudioChannel loop?
+        /// </summary>
+        bool _loop = false;
 
-    /// <summary>
-    /// Change volume each loop (when using randomized volume)?
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Randomize volume on each loop?")]
-    bool _changeVolumeEachLoop = false;
+        /// <summary>
+        /// Is this AudioChannel playing?
+        /// </summary>
+        bool _playing = false;
 
-    /// <summary>
-    /// Uniform volume to use with this channel.
-    /// </summary>
-    [SerializeField][Range(0f, 1f)]
-    [Tooltip("Uniform volume to use with this channel.")]
-    float _volume = 1f;
+        /// <summary>
+        /// List of AudioClips to play in this channel.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("List of AudioClips to play in this channel.")]
+        List<AudioClip> _clips = new List<AudioClip>();
 
-    /// <summary>
-    /// Randomize volume?
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Randomize volume?")]
-    bool _randomVolume = false;
+        /// <summary>
+        /// Change volume each loop (when using randomized volume)?
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Randomize volume on each loop?")]
+        bool _changeVolumeEachLoop = false;
 
-    /// <summary>
-    /// Range of values to use for volume.
-    /// </summary>
-    [SerializeField][FloatRange (0f, 1f)]
-    [Tooltip("Range of values to use for volume.")]
-    FloatRange _volumeRange = new FloatRange();
+        /// <summary>
+        /// Uniform volume to use with this channel.
+        /// </summary>
+        [SerializeField]
+        [Range(0f, 1f)]
+        [Tooltip("Uniform volume to use with this channel.")]
+        float _volume = 1f;
 
-    /// <summary>
-    /// Timer for playback looping.
-    /// </summary>
-    float _loopTimer;
+        /// <summary>
+        /// Randomize volume?
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Randomize volume?")]
+        bool _randomVolume = false;
 
-    #endregion
-    #region Unity Callbacks
+        /// <summary>
+        /// Range of values to use for volume.
+        /// </summary>
+        [SerializeField]
+        [FloatRange(0f, 1f)]
+        [Tooltip("Range of values to use for volume.")]
+        FloatRange _volumeRange = new FloatRange();
 
-    private void OnEnable() {
-        _audioSource = GetComponentInParent<AudioSource>();
-        if (_parent == null)
-            _parent = GetComponentInParent<AudioGroup>();
-    }
+        /// <summary>
+        /// Timer for playback looping.
+        /// </summary>
+        float _loopTimer;
 
-    private void Update() {
-        if (_loop && _playing) {
-            _loopTimer -= Time.deltaTime;
-            if (_loopTimer <= 0f) {
-                LoopSound();
+        #endregion
+        #region Unity Callbacks
+
+        private void OnEnable() {
+            _audioSource = GetComponentInParent<AudioSource>();
+            if (_parent == null)
+                _parent = GetComponentInParent<AudioGroup>();
+        }
+
+        private void Update() {
+            if (_loop && _playing) {
+                _loopTimer -= Time.deltaTime;
+                if (_loopTimer <= 0f) {
+                    LoopSound();
+                }
             }
         }
-    }
 
-    #endregion
-    #region Properties
+        #endregion
+        #region Properties
 
-    /// <summary>
-    /// Returns all AudioClips used in this AudioChannel (read-only).
-    /// </summary>
-    public List<AudioClip> Clips { get { return _clips; } }
+        /// <summary>
+        /// Returns this AudioChannel's parent AudioGroup (read-only).
+        /// </summary>
+        public AudioGroup Parent { get { return _parent; } }
 
-    /// <summary>
-    /// Gets/sets the uniform volume to use.
-    /// </summary>
-    public float Volume {
-        get { return _volume; }
-        set { _volume = value; }
-    }
+        /// <summary>
+        /// Returns all AudioClips used in this AudioChannel (read-only).
+        /// </summary>
+        public List<AudioClip> Clips { get { return _clips; } }
 
-    /// <summary>
-    /// Gets/sets the range of volumes to use.
-    /// </summary>
-    public FloatRange VolumeRange {
-        get { return _volumeRange; }
-        set { _volumeRange = value; }
-    }
-
-    /// <summary>
-    /// Gets/sets whether or not to randomize volume.
-    /// </summary>
-    public bool UseRandomVolume {
-        get { return _randomVolume; }
-        set { _randomVolume = value; }
-    }
-
-    /// <summary>
-    /// Gets/sets whether or not to randomize volume on each loop.
-    /// </summary>
-    public bool ChangeVolumeEachLoop {
-        get { return _changeVolumeEachLoop; }
-        set { _changeVolumeEachLoop = value; }
-    }
-
-    #endregion
-    #region Methods
-
-    /// <summary>
-    /// Sets the parent AudioGroup of this AudioChannel.
-    /// </summary>
-    /// <param name="parent"></param>
-    public void SetParent (AudioGroup parent) {
-        _parent = parent;
-    }
-
-    /// <summary>
-    /// Plays a sound from this channel.
-    /// </summary>
-    public void PlaySound (float pitch, bool loop=false) {
-        if (_clips.Count <= 0) return;
-
-        _loop = loop;
-        _audioSource.volume = _randomVolume ? _volumeRange.GetRandomValue() : _volume;
-        _audioSource.pitch = pitch;
-
-        var clip = _clips.GetRandom();
-        _audioSource.PlayOneShot (clip);
-
-        if (_loop) {
-            _loopTimer = clip.length;
-            _playing = true;
+        /// <summary>
+        /// Gets/sets the uniform volume to use.
+        /// </summary>
+        public float Volume {
+            get { return _volume; }
+            set { _volume = value; }
         }
-    }
 
-    /// <summary>
-    /// Loops the sound.
-    /// </summary>
-    void LoopSound () {
-        var clip = _clips.GetRandom();
-        _audioSource.PlayOneShot (clip);
-        _loopTimer = clip.length;
-    }
+        /// <summary>
+        /// Gets/sets the range of volumes to use.
+        /// </summary>
+        public FloatRange VolumeRange {
+            get { return _volumeRange; }
+            set { _volumeRange = value; }
+        }
 
-    /// <summary>
-    /// Stops this AudioChannel's playback.
-    /// </summary>
-    public void Stop (){
-        _audioSource.Stop();
-        _playing = false;
-    }
+        /// <summary>
+        /// Gets/sets whether or not to randomize volume.
+        /// </summary>
+        public bool UseRandomVolume {
+            get { return _randomVolume; }
+            set { _randomVolume = value; }
+        }
 
-    #endregion
+        /// <summary>
+        /// Gets/sets whether or not to randomize volume on each loop.
+        /// </summary>
+        public bool ChangeVolumeEachLoop {
+            get { return _changeVolumeEachLoop; }
+            set { _changeVolumeEachLoop = value; }
+        }
+
+        #endregion
+        #region Methods
+
+        /// <summary>
+        /// Sets the parent AudioGroup of this AudioChannel.
+        /// </summary>
+        /// <param name="parent"></param>
+        public void SetParent(AudioGroup parent) {
+            _parent = parent;
+        }
+
+        /// <summary>
+        /// Plays a sound from this channel.
+        /// </summary>
+        public void PlaySound(float pitch, bool loop = false) {
+            if (_clips.Count <= 0) return;
+
+            _loop = loop;
+            _audioSource.volume = _randomVolume ? _volumeRange.GetRandomValue() : _volume;
+            _audioSource.pitch = pitch;
+
+            var clip = _clips.GetRandom();
+            _audioSource.PlayOneShot(clip);
+
+            if (_loop) {
+                _loopTimer = clip.length;
+                _playing = true;
+            }
+        }
+
+        /// <summary>
+        /// Loops the sound.
+        /// </summary>
+        void LoopSound() {
+            var clip = _clips.GetRandom();
+            _audioSource.PlayOneShot(clip);
+            _loopTimer = clip.length;
+        }
+
+        /// <summary>
+        /// Stops this AudioChannel's playback.
+        /// </summary>
+        public void Stop() {
+            _audioSource.Stop();
+            _playing = false;
+        }
+
+        #endregion
+    }
 }
--- Assets/Scripts/Audio/AudioGroup.cs
+++ Assets/Scripts/Audio/AudioGroup.cs
@@ -4,252 +4,273 @@ using System.Collections.Generic;
 using UnityEngine;
 using ModMan;
 
-/// <summary>
-/// Audio class to emulate FMOD-style functionality.
-/// </summary>
-[ExecuteInEditMode]
-[RequireComponent(typeof(AudioSource), typeof(AudioChannel))]
-public class AudioGroup : MonoBehaviour {
-
-    #region Consts
-
-    const string _STANDARD_CHANNEL_NAME = "STANDARD";
-    const string _BASS_CHANNEL_NAME     = "BASS";
-    const string _MID_CHANNEL_NAME      = "MID";
-    const string _TREBLE_CHANNEL_NAME   = "TREBLE";
-    const string _ELEMENTS_PARENT_NAME  = "ELEMENTS";
-
-    #endregion
-    #region Vars
+namespace Turing.Audio {
 
     /// <summary>
-    /// Type of group setup to use.
+    /// Audio class to emulate FMOD-style functionality.
     /// </summary>
-    [SerializeField]
-    [Tooltip("Type of group setup to use.")]
-    GroupType _groupType = GroupType.Standard;
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(AudioSource), typeof(AudioChannel))]
+    public class AudioGroup : MonoBehaviour {
 
-    /// <summary>
-    /// AudioChannel object for standard (non-layered) playback.
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Audio channel object for standard (non-layered) playback.")]
-    AudioChannel _standardChannel;
+        #region Consts
 
-    /// <summary>
-    /// AudioChannel objects for layered playback.
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Audio channel objects for layered playback.")]
-    AudioChannel _bassChannel, _midChannel, _trebleChannel;
+        const string _STANDARD_CHANNEL_NAME = "STANDARD";
+        const string _BASS_CHANNEL_NAME = "BASS";
+        const string _MID_CHANNEL_NAME = "MID";
+        const string _TREBLE_CHANNEL_NAME = "TREBLE";
+        const string _ELEMENTS_PARENT_NAME = "ELEMENTS";
 
-    /// <summary>
-    /// GameObject to which to parent element AudioChannels.
-    /// </summary>
-    Transform _elementParent;
+        #endregion
+        #region Vars
 
-    /// <summary>
-    /// List of element AudioChannels.
-    /// </summary>
-    [SerializeField]
-    [Tooltip("List of element AudioChannels.")]
-    List<AudioChannel> _elementChannels = new List<AudioChannel>();
+        /// <summary>
+        /// Type of group setup to use.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Type of group setup to use.")]
+        GroupType _groupType = GroupType.Standard;
 
-    /// <summary>
-    /// Will playback loop?
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Will playback loop?")]
-    bool _loop = false;
+        /// <summary>
+        /// AudioChannel object for standard (non-layered) playback.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Audio channel object for standard (non-layered) playback.")]
+        AudioChannel _standardChannel;
 
-    /// <summary>
-    /// Change pitch each loop (when using randomized pitch)?
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Change pitch each loop (when using randomized pitch)?")]
-    bool _changePitchEachLoop = false;
+        /// <summary>
+        /// AudioChannel objects for layered playback.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Audio channel objects for layered playback.")]
+        AudioChannel _bassChannel, _midChannel, _trebleChannel;
 
-    /// <summary>
-    /// If looped, is this AudioGroup currently playing?
-    /// </summary>
-    bool _playing = false;
+        /// <summary>
+        /// GameObject to which to parent element AudioChannels.
+        /// </summary>
+        Transform _elementParent;
 
-    /// <summary>
-    /// Randomize volume on each playback?
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Randomize volume on each playback?")]
-    bool _randomVolume = false;
+        /// <summary>
+        /// List of element AudioChannels.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("List of element AudioChannels.")]
+        List<AudioChannel> _elementChannels = new List<AudioChannel>();
 
-    /// <summary>
-    /// Uniform volume to use.
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Uniform volume to use.")]
-    float _volume = 1f;
+        /// <summary>
+        /// Will playback loop?
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Will playback loop?")]
+        bool _loop = false;
 
-    /// <summary>
-    /// Range of volumes to randomize.
-    /// </summary>
-    [SerializeField][FloatRange (0f, 1f)]
-    [Tooltip("Range of volumes to randomize.")]
-    FloatRange _volumeRange = new FloatRange();
+        /// <summary>
+        /// Change pitch each loop (when using randomized pitch)?
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Change pitch each loop (when using randomized pitch)?")]
+        bool _changePitchEachLoop = false;
 
-    /// <summary>
-    /// Randomize pitch on each playback?
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Randomize pitch on each playback?")]
-    bool _randomPitch = false;
+        /// <summary>
+        /// If looped, is this AudioGroup currently playing?
+        /// </summary>
+        bool _playing = false;
 
-    /// <summary>
-    /// Uniform pitch to use.
-    /// </summary>
-    [SerializeField][Range(0f, 3f)]
-    [Tooltip("Uniform pitch to use.")]
-    float _pitch = 1f;
+        /// <summary>
+        /// Randomize volume on each playback?
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Randomize volume on each playback?")]
+        bool _randomVolume = false;
 
-    /// <summary>
-    /// Range of pitches to use.
-    /// </summary>
-    [SerializeField][FloatRange(0f, 3f)]
-    [Tooltip("Range of pitches to use.")]
-    FloatRange _pitchRange = new FloatRange();
+        /// <summary>
+        /// Uniform volume to use.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Uniform volume to use.")]
+        float _volume = 1f;
 
-    /// <summary>
-    /// Index of next element channel (for naming).
-    /// </summary>
-    [SerializeField]
-    int _elementChannelIndex = 0;
+        /// <summary>
+        /// Range of volumes to randomize.
+        /// </summary>
+        [SerializeField]
+        [FloatRange(0f, 1f)]
+        [Tooltip("Range of volumes to randomize.")]
+        FloatRange _volumeRange = new FloatRange();
 
-    #endregion
-    #region Unity Callbacks
+        /// <summary>
+        /// Randomize pitch on each playback?
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Randomize pitch on each playback?")]
+        bool _randomPitch = false;
 
-    private void OnEnable() {
-        var channels = GetComponentsInChildren<AudioChannel>();
-        if (channels == null) {
-            Debug.LogWarning ("No AudioChannels found on this AudioGroup! Generating now.", gameObject);
-            GenerateAudioChannels();
-        } else if (InvalidAudioChannels()) {
-            Debug.LogWarning ("Fixing AudioChannels...", gameObject);
-            GenerateAudioChannels();
+        /// <summary>
+        /// Uniform pitch to use.
+        /// </summary>
+        [SerializeField]
+        [Range(0f, 3f)]
+        [Tooltip("Uniform pitch to use.")]
+        float _pitch = 1f;
+
+        /// <summary>
+        /// Range of pitches to use.
+        /// </summary>
+        [SerializeField]
+        [FloatRange(0f, 3f)]
+        [Tooltip("Range of pitches to use.")]
+        FloatRange _pitchRange = new FloatRange();
+
+        /// <summary>
+        /// Index of next element channel (for naming).
+        /// </summary>
+        [SerializeField]
+        int _elementChannelIndex = 0;
+
+        #endregion
+        #region Unity Callbacks
+
+        private void OnEnable() {
+            var channels = GetComponentsInChildren<AudioChannel>();
+            if (channels == null) {
+                Debug.LogWarning("No AudioChannels found on this AudioGroup! Generating now.", gameObject);
+                GenerateAudioChannels();
+            } else if (InvalidAudioChannels()) {
+                Debug.LogWarning("Fixing AudioChannels...", gameObject);
+                GenerateAudioChannels();
+            }
         }
-    }
 
-    #endregion
-    #region Methods
+        #endregion
+        #region Properties
 
-    /// <summary>
-    /// Plays this AudioElement.
-    /// </summary>
-    public void Play () {
-        if (_loop) _playing = true;
-        var pitch = _randomPitch ? _pitchRange.GetRandomValue() : _pitch;
+        /// <summary>
+        /// Returns true if this AudioGroup loops (read-only).
+        /// </summary>
+        public bool Loop { get { return _loop; } }
 
-        switch (_groupType) {
-            case GroupType.Standard:
-                _standardChannel.PlaySound(pitch);
-                break;
-            case GroupType.Layered:
-                _bassChannel.PlaySound(pitch);
-                _midChannel.PlaySound(pitch);
-                _trebleChannel.PlaySound(pitch);
-                break;
-            case GroupType.Elements:
-                foreach (var channel in _elementChannels)
-                    channel.PlaySound(pitch);
-                break;
+        #endregion
+        #region Methods
+
+        /// <summary>
+        /// Plays this AudioElement.
+        /// </summary>
+        public void Play() {
+            if (_loop) _playing = true;
+            var pitch = _randomPitch ? _pitchRange.GetRandomValue() : _pitch;
+
+            switch (_groupType) {
+                case GroupType.Standard:
+                    _standardChannel.PlaySound(pitch);
+                    break;
+                case GroupType.Layered:
+                    _bassChannel.PlaySound(pitch);
+                    _midChannel.PlaySound(pitch);
+                    _trebleChannel.PlaySound(pitch);
+                    break;
+                case GroupType.Elements:
+                    foreach (var channel in _elementChannels)
+                        channel.PlaySound(pitch);
+                    break;
+            }
         }
-    } 
 
-    /// <summary>
-    /// Stops all playback on this AudioElement.
-    /// </summary>
-    public void Stop () {
-        _playing = false;
+        /// <summary>
+        /// Stops all playback on this AudioElement.
+        /// </summary>
+        public void Stop() {
+            _playing = false;
 
-        switch (_groupType) {
-            case GroupType.Standard:
-                _standardChannel.Stop();
-                break;
-            case GroupType.Layered:
-                _bassChannel.Stop();
-                _midChannel.Stop();
-                _trebleChannel.Stop();
-                break;
-            case GroupType.Elements:
-                foreach (var channel in _elementChannels)
-                    channel.Stop();
-                break;
+            switch (_groupType) {
+                case GroupType.Standard:
+                    _standardChannel.Stop();
+                    break;
+                case GroupType.Layered:
+                    _bassChannel.Stop();
+                    _midChannel.Stop();
+                    _trebleChannel.Stop();
+                    break;
+                case GroupType.Elements:
+                    foreach (var channel in _elementChannels)
+                        channel.Stop();
+                    break;
+            }
         }
-    }
 
-    /// <summary>
-    /// Generates all audio channels with default settings.
-    /// </summary>
-    void GenerateAudioChannels () {
-        _standardChannel = GenerateAudioChannel(_STANDARD_CHANNEL_NAME, transform);
-        _bassChannel     = GenerateAudioChannel (_BASS_CHANNEL_NAME, transform);
-        _midChannel      = GenerateAudioChannel (_MID_CHANNEL_NAME, transform);
-        _trebleChannel   = GenerateAudioChannel (_TREBLE_CHANNEL_NAME, transform);
-        if (_elementParent == null) {
-            _elementParent = new GameObject(_ELEMENTS_PARENT_NAME).transform;
-            _elementParent.hideFlags = HideFlags.HideInHierarchy;
-            _elementParent.SetParent (transform);
-            _elementParent.Reset();
+        /// <summary>
+        /// Generates all audio channels with default settings.
+        /// </summary>
+        void GenerateAudioChannels() {
+            _standardChannel = GenerateAudioChannel(_STANDARD_CHANNEL_NAME, transform);
+            _bassChannel = GenerateAudioChannel(_BASS_CHANNEL_NAME, transform);
+            _midChannel = GenerateAudioChannel(_MID_CHANNEL_NAME, transform);
+            _trebleChannel = GenerateAudioChannel(_TREBLE_CHANNEL_NAME, transform);
+            if (_elementParent == null) {
+                _elementParent = new GameObject(_ELEMENTS_PARENT_NAME).transform;
+                _elementParent.hideFlags = HideFlags.HideInHierarchy;
+                _elementParent.SetParent(transform);
+                _elementParent.Reset();
+            }
         }
-    }
 
-    /// <summary>
-    /// Generates a default audio channel with the given name.
-    /// </summary>
-    AudioChannel GenerateAudioChannel (string channelName, Transform parent) {
-        var channelObj = gameObject.FindInChildren (channelName);
-        if (channelObj == null) {
-            channelObj = new GameObject (
-                channelName, 
-                typeof (AudioSource),
-                typeof (AudioChannel)
-                );
-            channelObj.hideFlags = HideFlags.HideInHierarchy;
-            channelObj.transform.SetParent (parent);
-            channelObj.transform.Reset();
-        } else {
-            var channel = channelObj.GetComponent<AudioChannel>();
-            if (channel == null) return channelObj.AddComponent<AudioChannel>();
+        /// <summary>
+        /// Generates a default audio channel with the given name.
+        /// </summary>
+        AudioChannel GenerateAudioChannel(string channelName, Transform parent) {
+            var channelObj = gameObject.FindInChildren(channelName);
+            if (channelObj == null) {
+                channelObj = new GameObject(
+                    channelName,
+                    typeof(AudioSource),
+                    typeof(AudioChannel)
+                    );
+                channelObj.hideFlags = HideFlags.HideInHierarchy;
+                channelObj.transform.SetParent(parent);
+                channelObj.transform.Reset();
+            } else {
+                var channel = channelObj.GetComponent<AudioChannel>();
+                if (channel == null) return channelObj.AddComponent<AudioChannel>();
+            }
+            return channelObj.GetComponent<AudioChannel>();
         }
-        return channelObj.GetComponent<AudioChannel>();
+
+        /// <summary>
+        /// Adds an element channel to this AudioGroup.
+        /// </summary>
+        public void AddElementChannel() {
+            var channel = GenerateAudioChannel(_ELEMENTS_PARENT_NAME + _elementChannelIndex++, _elementParent.transform);
+            _elementChannels.Add(channel);
+        }
+
+        /// <summary>
+        /// Removes an elementchannel from this AudioGroup.
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveElementChannel(int index) {
+            _elementChannels.RemoveAt(index);
+        }
+
+        /// <summary>
+        /// Returns true if any audio channels are invalid.
+        /// </summary>
+        /// <returns></returns>
+        bool InvalidAudioChannels() {
+            return _standardChannel == null || _bassChannel == null ||
+                _midChannel == null || _trebleChannel == null || _elementParent == null;
+        }
+
+        #endregion
+        #region Enums
+
+        /// <summary>
+        /// Enum for type of AudioGroup.
+        /// </summary>
+        public enum GroupType {
+            Standard = 0, // Single channel
+            Layered = 1, // Bass/mid/treble channels
+            Elements = 2  // Per-element channels
+        }
+
+        #endregion
     }
-
-    public void AddElementChannel() {
-        var channel = GenerateAudioChannel (_ELEMENTS_PARENT_NAME + _elementChannelIndex++, _elementParent.transform);
-        _elementChannels.Add (channel);
-    }
-
-    public void RemoveElementChannel (int index) {
-        _elementChannels.RemoveAt (index);
-    }
-
-    /// <summary>
-    /// Returns true if any audio channels are invalid.
-    /// </summary>
-    /// <returns></returns>
-    bool InvalidAudioChannels () {
-        return _standardChannel == null || _bassChannel == null || 
-            _midChannel == null || _trebleChannel == null || _elementParent == null;
-    }
-
-    #endregion
-    #region Enums
-
-    /// <summary>
-    /// Enum for type of AudioGroup.
-    /// </summary>
-    public enum GroupType {
-        Standard = 0, // Single channel
-        Layered  = 1, // Bass/mid/treble channels
-        Elements = 2  // Per-element channels
-    }
-
-    #endregion
 }
