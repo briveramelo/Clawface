2cd4469c0c7230246eef7aa7b3df9b8eb7f8f7f7
Aaron Desin <aaron@aarondesin.com>
2017-03-29 15:30:35 -0600

    Light scaler

---
+++ Assets/TrailerLightManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerLightManager : MonoBehaviour {
+
+	Dictionary<Light, float> _intensities = new Dictionary<Light, float>();
+
+    [SerializeField] AnimationCurve _lightScaler;
+
+    private void Awake() {
+        var lights = FindObjectsOfType<Light>();
+        foreach (var light in lights) {
+            _intensities.Add (light, light.intensity);
+        }
+    }
+
+    private void Update() {
+        ScaleLights (_lightScaler.Evaluate (Time.realtimeSinceStartup));
+    }
+
+    void ScaleLights (float value) {
+        foreach (var light in _intensities.Keys) {
+            light.intensity = _intensities[light] * value;
+        }
+    }
+}
