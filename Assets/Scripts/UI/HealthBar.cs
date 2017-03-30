d484617653e89ef46d40d72819f7f3dfd98e8e3d
Cornelia Xaos <cornelia.xaos@gmail.com>
2017-03-30 11:57:54 -0600

    Cleaned up some code and patched a UI thing.

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -7,23 +7,27 @@ using UnityEngine.Assertions;
 
 public class HealthBar : Singleton<HealthBar> {
     
-    protected HealthBar() { }
 
-    //// Unity Inspector Fields
+    #region Unity Inspector Fields
     [SerializeField]
     private Transform mask, bar;
+    #endregion
 
-    //// HealthBar Public Interface
+    #region Public Interface
     public void SetHealth(float health)
     {
         Assert.IsTrue(health >= 0.0F && health <= 1.0F);
         mask.localScale = new Vector3(health, 1.0F, 1.0F);
         bar.localScale = new Vector3(health == 0 ? 0 : 1 / health, 1.0F, 1.0F);
     }
+    #endregion
+
+    #region Protected Interface
+    protected HealthBar() { }
 
     protected override void Awake()
     {
         base.Awake();
- 
     }
+    #endregion
 }
