9eb47bb4e8b35341faf23386070abbb4e7c538fe
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2017-04-04 13:16:49 -0600

    player_combat prefab updated for aaron

--- Assets/Scripts/Effects/ParticleAttractor.cs
+++ Assets/Scripts/Effects/ParticleAttractor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-using UnityEditor;
+//using UnityEditor;
 
 [RequireComponent(typeof(ParticleSystem))]
 public class ParticleAttractor : MonoBehaviour {
