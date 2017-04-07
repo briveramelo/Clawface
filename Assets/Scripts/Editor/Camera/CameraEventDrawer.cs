ca0b5630be8c4a5b1ed16a702dfd779b86e6ff0e
Aaron Desin <aaron@aarondesin.com>
2017-04-07 14:00:21 -0600

    Improvements to camera tool (timeline scrubbing, better UI)

--- Assets/Scripts/Editor/Camera/CameraEventDrawer.cs
+++ Assets/Scripts/Editor/Camera/CameraEventDrawer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.Events;
 using ModMan;
 
 [CustomPropertyDrawer(typeof(CameraTrack.CameraEvent))]
@@ -9,20 +10,32 @@ public class CameraEventDrawer : PropertyDrawer {
 
     CameraTrack.CameraEvent _event;
 
-    const float _TIME_FIELD_PERCENTAGE = 0.5f;
+    const float _TIME_LABEL_WIDTH = 48f;
+    const float _TIME_FIELD_HEIGHT = 16f;
+    const float _TIME_FIELD_WIDTH = 64f;
+    const float _PADDING = 4f;
+    const float _EVENT_FIELD_WIDTH = 128f;
+    const float _EVENT_FIELD_HEIGHT_BASE = 84f;
+    const float _EVENT_FIELD_EVENT_HEIGHT = 44f;
+    public const float REMOVE_EVENT_BUTTON_WIDTH = 16f;
+
+    int _listeners = 0;
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-        return 128f;
+        return _EVENT_FIELD_HEIGHT_BASE + (_listeners > 0 ? _listeners - 1 : _listeners) * _EVENT_FIELD_EVENT_HEIGHT;
     }
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
         var time = property.FindPropertyRelative ("time");
         var e = property.FindPropertyRelative ("onTrigger");
+        _listeners = e.FindPropertyRelative("m_PersistentCalls.m_Calls").arraySize;
 
-        float w = position.width * _TIME_FIELD_PERCENTAGE;
+        var x = position.x;
 
-        EditorGUI.PropertyField (new Rect (position.x, position.y, w, position.height), time);
-        EditorGUI.PropertyField (new Rect (position.x + w, position.y, position.width - w, position.height), e);
+        //float w = position.width * _TIME_FIELD_PERCENTAGE;
+        EditorGUI.LabelField (new Rect (x, position.y, _TIME_LABEL_WIDTH, _TIME_FIELD_HEIGHT), "Time");
+        EditorGUI.PropertyField (new Rect (position.x += _TIME_LABEL_WIDTH, position.y, _TIME_FIELD_WIDTH, _TIME_FIELD_HEIGHT), time, GUIContent.none);
+        EditorGUI.PropertyField (new Rect (position.x += _TIME_FIELD_WIDTH + _PADDING, position.y, position.width - _TIME_FIELD_WIDTH - _TIME_LABEL_WIDTH - _PADDING, position.height), e);
 
         //base.OnGUI(position, property, label);
     }
