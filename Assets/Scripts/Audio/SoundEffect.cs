5aca502b84d2eb1ee8285ade02333029609c0c6e
Chuan-Chin Lai <danny6430@hotmail.com>
2017-04-10 15:13:33 -0600

    updated SFX Manager

---
+++ Assets/Scripts/Audio/SoundEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SoundEffect
+{
+    protected GameObject m_SFX_GameObject = null;
+    protected Turing.Audio.AudioGroup m_AudioGroup = null;
+
+    public SoundEffect(GameObject i_SFX)
+    {
+        m_SFX_GameObject = i_SFX;
+        m_AudioGroup = m_SFX_GameObject.GetComponent<Turing.Audio.AudioGroup>();
+    }
+
+    public virtual void Play()
+    {
+        m_AudioGroup.Play();
+    }
+}
