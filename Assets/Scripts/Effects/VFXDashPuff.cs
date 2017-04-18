d6237805505df00019b860924000d620631412e3
Aaron Desin <aaron@aarondesin.com>
2017-04-17 21:10:53 -0600

    Dash effects

---
+++ Assets/Scripts/Effects/VFXDashPuff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXDashPuff : MonoBehaviour {
+
+	ParticleSystem[] _ps;
+
+    private void Awake() {
+        _ps = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    public void Play () {
+        foreach (var ps in _ps) ps.Play();
+    }
+
+    public void Stop () {
+        foreach (var ps in _ps) ps.Stop();
+    }
+}
