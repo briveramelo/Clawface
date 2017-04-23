45ad67039d3e669122c5b22d395440fa1e8bd88d
Bharat Gudihal <bharat.gudi@gmail.com>
2017-04-22 19:26:43 -0600

    grappler foot functionality

--- Assets/Scripts/Helpers/VelocityBody.cs
+++ Assets/Scripts/Helpers/VelocityBody.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using MovementEffects;
-using System;
 
 public class VelocityBody : MonoBehaviour, IMovable{
 
