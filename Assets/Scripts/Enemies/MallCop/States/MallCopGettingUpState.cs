42f08a51855036649b2f66c96f5e8639f6267898
Aaron Desin <aaron@aarondesin.com>
2017-04-23 23:30:53 -0600

    Mall cop ragdolls

---
+++ Assets/Scripts/Enemies/MallCop/States/MallCopGettingUpState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MovementEffects;
+
+public class MallCopGettingUpState : MallCopState {
+
+    bool isGettingUp = false;
+
+    public override void OnEnter() {
+        animator.SetInteger(Strings.ANIMATIONSTATE, (int)MallCopAnimationStates.GettingUp);
+        Timing.RunCoroutine (RunStartupTimer());
+    }
+
+    public override void OnExit() {}
+
+    public override void Update() {}
+
+    public bool IsDoneGettingUp {
+        get {
+            return CanRestart();
+        }
+    }
+
+    IEnumerator<float> RunStartupTimer() {
+        isPastStartup = false;
+        yield return Timing.WaitForSeconds(.2f);
+        isPastStartup = true;
+    }
+    bool isPastStartup;
+
+    public bool CanRestart() {
+        return animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.85f && isPastStartup;
+    }
+}
---
+++ Assets/Scripts/Enemies/MallCop/States/MallCopIdleState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MallCopIdleState : MallCopState {
+
+    public override void OnEnter() {}
+
+    public override void OnExit() {}
+
+    public override void Update() {}
+}
