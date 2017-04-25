7653b4f1ca98bd165c5c03f8d99472389e1c1957
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2017-04-25 15:51:23 -0600

    Health gain effect in and grappler mod force tuned

--- Assets/Scripts/Character/States/SkinningState.cs
+++ Assets/Scripts/Character/States/SkinningState.cs
@@ -52,9 +52,8 @@ public class SkinningState : IPlayerState
             skinningEffect.transform.position = transform.position;
 
             GameObject healthJuice = ObjectPool.Instance.GetObject(PoolObjectType.HealthGain);
-            if (healthJuice) {
-                healthJuice.transform.position = transform.position + Vector3.up * 3.2f;
-                healthJuice.DeActivate(3f);
+            if (healthJuice) {                                
+                healthJuice.FollowAndDeActivate(3f, transform, Vector3.up * 3.2f);
             }
         }        
         stateVariables.stateFinished = true;
