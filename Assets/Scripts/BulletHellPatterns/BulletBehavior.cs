ff86faec21d5a53340781fc386926ea6894f07f2
JPsLucky13 <jeanpaulpeschard43@gmail.com>
2017-09-05 18:30:15 -0600

    Added Turret prefab

--- Assets/Scripts/BulletHellPatterns/BulletBehavior.cs
+++ Assets/Scripts/BulletHellPatterns/BulletBehavior.cs
@@ -1,23 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MovementEffects;
 
 public class BulletBehavior : MonoBehaviour {
 
     Vector3 movementVector;
     float bulletSpeed;
 
+    private ShooterProperties shooterProperties = new ShooterProperties();
+    private Damager damager = new Damager();
+    private bool shooter;
 
     private void Start()
     {
-        
+        shooterProperties.Initialize(0, 5, 6, 2);
+        SetShooterProperties(shooterProperties);
+        Timing.RunCoroutine(DestroyAfter());
     }
+
+    private IEnumerator<float> DestroyAfter()
+    {
+        yield return Timing.WaitForSeconds(3f);
+        if (gameObject)
+        {
+            EmitBulletCollision();
+            Destroy(gameObject);
+        }
+    }
+
 
 
     public void AssignBulletValues(Vector3 forwardDirection, float speed)
     {
         movementVector = forwardDirection;
         bulletSpeed = speed;
+        
     }
 
     void Update()
@@ -25,6 +43,74 @@ public class BulletBehavior : MonoBehaviour {
         transform.position += movementVector * bulletSpeed * Time.deltaTime;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+            bool isEnemy = other.gameObject.CompareTag(Strings.Tags.ENEMY);
+            bool isPlayer = other.gameObject.CompareTag(Strings.Tags.PLAYER);
+            if (isEnemy || isPlayer)
+            {
+                Damage(other.gameObject.GetComponent<IDamageable>());
+                Push(other.gameObject.GetComponent<IMovable>());
+            }
+            if (isEnemy || isPlayer || other.gameObject.layer == (int)Layers.Ground)
+            {
+                SFXManager.Instance.Play(SFXType.BlasterProjectileImpact, transform.position);
+                EmitBulletCollision();
+                Destroy(gameObject);
+            }
+    }
+
+    public void SetShooterProperties(ShooterProperties shooterProperties)
+    {
+        this.shooterProperties = shooterProperties;
+    }
+
+
+    private void Damage(IDamageable damageable)
+    {
+        if (damageable != null)
+        {
+
+            // Shooter is player
+            if (!shooter)
+            {
+                AnalyticsManager.Instance.AddModDamage(ModType.ArmBlaster, shooterProperties.damage);
+
+                if (damageable.GetHealth() - shooterProperties.damage <= 0.01f)
+                {
+                    AnalyticsManager.Instance.AddModKill(ModType.ArmBlaster);
+                }
+            }
+            else
+            {
+                AnalyticsManager.Instance.AddEnemyModDamage(ModType.ArmBlaster, shooterProperties.damage);
+            }
+            damager.Set(shooterProperties.damage, DamagerType.BlasterBullet, transform.forward);
+            damageable.TakeDamage(damager);
+        }
+    }
+
+    private void Push(IMovable movable)
+    {
+        Vector3 forceDirection = transform.forward;
+        if (movable != null)
+        {
+            movable.AddDecayingForce(forceDirection.normalized * shooterProperties.pushForce);
+        }
+    }
+    private void EmitBulletCollision()
+    {
+        GameObject effect = ObjectPool.Instance.GetObject(PoolObjectType.BlasterImpactEffect);
+        if (effect)
+        {
+            effect.transform.position = transform.position;
+        }
+    }
+
+    public void SetWielderInstanceID(int id)
+    {
+        shooterProperties.shooterInstanceID = id;
+    }
 
 
 }
--- Assets/Scripts/BulletHellPatterns/BulletHellPatternController.cs
+++ Assets/Scripts/BulletHellPatterns/BulletHellPatternController.cs
@@ -4,82 +4,76 @@ using UnityEngine;
 
 public class BulletHellPatternController : MonoBehaviour {
 
-    [SerializeField]
-    float bulletSpeed;
-    [SerializeField]
-    float rateOfFire;
+    #region 1. Serialized Unity Inspector Fields
+    [SerializeField][Range(-360.0f, 360.0f)]private float separationFromForwardVector;
+    [SerializeField][Range(0.0f, 100.0f)]   private float bulletSpeed;
+    [SerializeField][Range(0.0f, 10.0f)]    private float rateOfFire;
+    [SerializeField]                        private GameObject bulletPrefab;
+    [SerializeField][Range(0.0f, 30.0f)]    private int bulletStrands;
+    [SerializeField][Range(0.0f, 360.0f)]   private float separationAngleBetweenStrands;
+    [SerializeField]                        private RotateDirection rotationDirection;
+    [SerializeField][Range(0.0f, 10.0f)]    private float rotationSpeed;
+    #endregion
 
-    [SerializeField]
-    private GameObject bulletPrefab;
 
-    [SerializeField]
-    private int bulletStrands;
-
+    #region 2. Private fields
+    private Vector3 referenceVector;
+    private Quaternion rotationSeparationFromForward;
     private List<BulletHellStrand> bulletHellStrandList;
-
-    [SerializeField]
-    private float separationAngle;
-
-    //Rotation types
     private enum RotateDirection
     {
         Right,
         Left
     };
-
-    [SerializeField]
-    private RotateDirection rotationDirection;
-
-    [SerializeField]
-    private float rotationSpeed;
-
-
-    private Vector3 referenceVector;
-
     private float currentTime;
+    private int currentNumberOfStrands = 0;
+    private int oldNumberOfStrands = 0;
+    private bool createStrandList = true;
+    #endregion
 
+    #region 3. Unity Lifecycle
+    //Used for Initialization
     private void Start()
     {
         bulletHellStrandList = new List<BulletHellStrand>();
     }
 
-
     // Update is called once per frame
     void Update () {
 
-        //The reference vector for all other bullet directions
-        referenceVector = transform.forward;
+        CalculateReferenceVector();
 
         currentTime += Time.deltaTime;
 
         if (currentTime > rateOfFire)
         {
-
-            OrganizeBulletStrands(bulletStrands, separationAngle);
-
+            UpdateStrands(bulletStrands);
+            OrganizeBulletStrands(bulletStrands, separationAngleBetweenStrands);
             FireBullet();
-
             currentTime = 0.0f;
         }
 
         RotateBulletHellController();
 	}
 
+
+    #endregion
+
+    #region 4. Private Methods  
     private void FireBullet()
     {
-
+        //For each stand 
         for (int i = 0; i < bulletHellStrandList.Count; i++)
         {
+            SFXManager.Instance.Play(SFXType.BlasterShoot, transform.position);
             GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
             newBullet.AddComponent<BulletBehavior>().AssignBulletValues(bulletHellStrandList[i].movementDirection, bulletSpeed);
         }
     }
-
     private void RotateBulletHellController()
     {
         transform.Rotate(Vector3.up * rotationSpeed * RotateDirectionValue());
     }
-
     private float RotateDirectionValue()
     {
         if (rotationDirection == RotateDirection.Left)
@@ -88,38 +82,77 @@ public class BulletHellPatternController : MonoBehaviour {
         else
             return 1.0f;
     }
+    private void CalculateReferenceVector()
+    {
+        //The reference vector for all other bullet directions
+        referenceVector = transform.forward;
 
+        rotationSeparationFromForward = Quaternion.Euler(0, separationFromForwardVector, 0);
+
+        referenceVector = (rotationSeparationFromForward * transform.forward);
+    }
+    private void UpdateStrands(int numberOfStrands)
+    {
+        currentNumberOfStrands = numberOfStrands;
+
+        if (oldNumberOfStrands != currentNumberOfStrands)
+        {
+            bulletHellStrandList.Clear();
+            createStrandList = true;
+        }
+
+    }
     private void OrganizeBulletStrands(int numberOfStrands, float separation)
     {
-        
-            for (int i = 0; i < numberOfStrands; i++)
-            {
-                Quaternion rotation = Quaternion.identity;
-                if (i % 2 != 0)
-                {
-                rotation = Quaternion.Euler(0,-separation + (-separation * (i - 1)), 0);
-                }
-                else {
+
+        currentNumberOfStrands = numberOfStrands;
+        oldNumberOfStrands = currentNumberOfStrands;
+
+        for (int i = 0; i < currentNumberOfStrands; i++)
+        {
+            Quaternion rotation = Quaternion.identity;
 
                 if (i == 0)
-                    rotation = Quaternion.Euler(0, separation + (separation * i), 0);
-                else {
-                    rotation = Quaternion.Euler(0, separation + (separation * i -1), 0);
-                }
+                    rotation = Quaternion.Euler(0, 0, 0);
+                else
+                {
+                    rotation = Quaternion.Euler(0, separation * i, 0);
                 }
 
-            if (bulletStrands != bulletHellStrandList.Count)
+            if (createStrandList)
             {
-                BulletHellStrand strand = new BulletHellStrand(rotation * transform.forward);
+                BulletHellStrand strand = new BulletHellStrand(rotation * referenceVector);
                 bulletHellStrandList.Add(strand);
+                
             }
-            else {
-                bulletHellStrandList[i].SetMovement(rotation * transform.forward);
+            else
+            {
+                bulletHellStrandList[i].SetMovement(rotation * referenceVector);
             }
 
 
         }
-        
-    }
+        //Finished creating the new list
+        createStrandList = false;
 
+    }
+    #endregion
+}
+
+public class BulletHellStrand
+{
+    #region 1. Public fields
+    public Vector3 movementDirection;
+    #endregion
+
+    #region 1. Public methods
+    public BulletHellStrand(Vector3 movementDirection)
+    {
+        this.movementDirection = movementDirection;
+    }
+    public void SetMovement(Vector3 direction)
+    {
+        movementDirection = direction;
+    }
+    #endregion
 }
