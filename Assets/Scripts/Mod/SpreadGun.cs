487c46eb85ebbb0f219ecaa8d0a1922a21ec4b10
Bharat Gudihal <bharat.gudi@gmail.com>
2017-09-19 16:10:26 -0600

    started work on spread gun

---
+++ Assets/Scripts/Mod/SpreadGun.cs
@@ -0,0 +1,112 @@
+﻿
+using System;
+using UnityEngine;
+
+public class SpreadGun : Mod {
+
+#region Serialized fields
+    [SerializeField] private SpreadGunProperties gunProperties;
+    [SerializeField] private Transform bulletSpawnTransform;
+    #endregion
+
+#region Private fields
+    private float incrementAngle;
+#endregion
+
+    #region Unity lifecycle
+
+    // Use this for initialization
+    protected override void Awake()
+    {
+        setModType(ModType.ForceSegway);
+        if(gunProperties.numberOfBulletsInEachShot != 0)
+        {
+            incrementAngle = gunProperties.spreadAngle / (float)gunProperties.numberOfBulletsInEachShot;
+        }
+        base.Awake();       
+    }
+
+    protected override void Update()
+    {
+        if (wielderMovable != null)
+        {
+            transform.forward = wielderMovable.GetForward();
+        }
+    }
+    #endregion
+
+#region Public functions
+    public override void AttachAffect(ref Stats wielderStats, IMovable wielderMovable)
+    {
+        base.AttachAffect(ref wielderStats, wielderMovable);
+    }
+
+    public override void Activate(Action onCompleteCoolDown=null, Action onActivate=null)
+    {        
+        base.Activate(onCompleteCoolDown, onActivate);   
+    }
+
+    protected override void ActivateStandardArms()
+    {
+        Shoot();
+    }
+
+    public override void DeActivate()
+    {
+        
+    }
+
+    public override void DetachAffect()
+    {
+        base.DetachAffect();
+    }
+    #endregion
+
+#region Private functions
+    private void Shoot()
+    {
+        for (int i = 0; i < gunProperties.numberOfBulletsInEachShot; i++)
+        {
+            //Get bullet and initialize
+            GameObject bullet = ObjectPool.Instance.GetObject(PoolObjectType.SpreadGunBullet);
+            if (bullet)
+            {
+                bullet.transform.position = bulletSpawnTransform.position;
+                bullet.transform.forward = CalculateForward(i);
+                SpreadGunBullet spreadBullet = bullet.GetComponent<SpreadGunBullet>();
+                if (spreadBullet)
+                {
+                    spreadBullet.Init(gunProperties.bulletSpeed, gunProperties.bulletMaxLifeTime, gunProperties.bulletMaxDistance, gunProperties.bulletDamage);
+                }
+            }
+        }
+    }
+
+    private Vector3 CalculateForward(int count)
+    {
+        float rotationAngle = -gunProperties.spreadAngle / 2.0f + (incrementAngle * count);
+        return transform.forward;
+    }
+    #endregion
+
+    #region Public Structures
+    [Serializable]
+    public class SpreadGunProperties
+    {
+        [Range(0, 180)]
+        [Tooltip("How much you want this baby to spread?")]        
+        public float spreadAngle;
+        [Tooltip("The name is quite descriptive")]
+        public int numberOfBulletsInEachShot;
+        [Tooltip("How fast you want the bois to travel?")]
+        public float bulletSpeed;
+        [Tooltip("How long you want the bois to live?")]
+        public float bulletMaxLifeTime;
+        [Tooltip("How far you want the bois to go?")]
+        public float bulletMaxDistance;
+        [Tooltip("How much the bois gon rek?")]
+        public float bulletDamage;
+    }
+#endregion
+
+}
---
+++ Assets/Scripts/Projectiles/SpreadGunBullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpreadGunBullet : MonoBehaviour {
+
+#region Private variables
+    private float speed;
+    private float maxLifeTime;
+    private float maxDistance;
+    private float damage;
+    private bool isReady;
+#endregion
+
+    #region Unity lifecycle
+    // Use this for initialization
+    void Start () {
+        isReady = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isReady)
+        {
+
+        }
+	}
+    #endregion
+
+#region Public functions
+    public void Init(float speed, float maxLifeTime, float maxDistance, float damage)
+    {
+        this.speed = speed;
+        this.maxDistance = maxDistance;
+        this.maxLifeTime = maxLifeTime;
+        this.damage = damage;
+        isReady = true;
+    }
+#endregion
+
+}
