7a0a1c96bdd0b65d041376b79489ab969e7fbb88
Aaron Desin <aaron@aarondesin.com>
2017-09-20 12:24:48 -0600

    Fixed int range (with Brandon's help!)

--- Assets/Scripts/Editor/Classes/IntRangeDrawer.cs
+++ Assets/Scripts/Editor/Classes/IntRangeDrawer.cs
@@ -1,15 +1,15 @@
 ﻿// IntRangeDrawer.cs
-// Author: Aaron
+// Author: Brandon, Aaron
 
 using UnityEditor;
 
 using UnityEngine;
 
 /// <summary>
-/// Custom PropertyDrawer for FloatRange.
+/// Custom PropertyDrawer for IntRange attributes.
 /// </summary>
 [CustomPropertyDrawer(typeof(IntRangeAttribute))]
-public sealed class IntRangeDrawer : PropertyDrawer
+public class IntRangeDrawer : PropertyDrawer
 {
     #region Private Fields
 
@@ -17,45 +17,50 @@ public sealed class IntRangeDrawer : PropertyDrawer
     const float Y_DIV_VALUE = 0.5f;
 
     #endregion
-    #region Unity Lifecycle
-
-    public override void OnGUI(Rect position, SerializedProperty property, 
-        GUIContent label)
-    {
-        var attrib = attribute as IntRangeAttribute;
-        var min = property.FindPropertyRelative ("min");
-        var minValue = min.intValue;
-        var max = property.FindPropertyRelative ("max");
-        var maxValue = max.intValue;
-
-        var minFloat = (float)minValue;
-        var maxFloat = (float)maxValue;
-
-        var xDiv = position.width * X_DIV_VALUE;
-        var yDiv = position.height * Y_DIV_VALUE;
-
-        EditorGUI.LabelField (new Rect (position.x, position.y, xDiv, yDiv), label);
-
-        EditorGUI.LabelField (new Rect (position.x, position.y + yDiv, position.width, yDiv), min.intValue.ToString());
-        EditorGUI.LabelField (new Rect (position.x + position.width - 40f, position.y + yDiv, position.width, yDiv), max.intValue.ToString());
-        EditorGUI.MinMaxSlider (new Rect (position.x + 24f, position.y + yDiv, position.width - 56f, yDiv), ref minFloat, ref maxFloat, attrib.min, attrib.max);
-
-        EditorGUI.LabelField(new Rect (position.x + xDiv * 2f, position.y, xDiv, yDiv), "To:");
-        minValue = (int)Mathf.Clamp( EditorGUI.IntField(new Rect(position.x + xDiv + 30f, position.y, xDiv - 30f, yDiv), minValue), attrib.min, maxValue );
-        EditorGUI.LabelField(new Rect(position.x + xDiv * 2f, position.y, xDiv, yDiv), "To: " );
-        maxValue = (int)Mathf.Clamp(EditorGUI.IntField(new Rect(position.x + xDiv * 2f + 24f, position.y, xDiv - 24f, yDiv), maxValue), minValue, attrib.max);
-
-        min.intValue = minValue;
-        max.intValue = maxValue;
-    }
-
-    #endregion
-    #region Public Methods
+    #region Pubilc Methods
 
     public override float GetPropertyHeight(SerializedProperty property, 
         GUIContent label)
     {
         return base.GetPropertyHeight(property, label) + 16f;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, 
+        GUIContent label)
+    {
+        var attrib = attribute as IntRangeAttribute;
+        var min = property.FindPropertyRelative("min");
+        float minValue = min.intValue;
+        var max = property.FindPropertyRelative("max");
+        float maxValue = max.intValue;
+
+        var xDiv = position.width * X_DIV_VALUE;
+        var yDiv = position.height * Y_DIV_VALUE;
+
+        EditorGUI.LabelField(new Rect(position.x, position.y, xDiv, yDiv), label);
+
+        EditorGUI.LabelField(
+            new Rect(position.x, position.y + yDiv, position.width, yDiv), 
+            min.intValue.ToString("0"));
+        EditorGUI.LabelField(
+            new Rect(position.x + position.width - 60f, position.y + yDiv, position.width, yDiv), 
+            max.intValue.ToString("0"));
+        EditorGUI.MinMaxSlider(
+            new Rect(position.x + 24f, position.y + yDiv, position.width - 56f, yDiv), 
+            ref minValue, ref maxValue, attrib.min, attrib.max);
+
+        EditorGUI.LabelField(new Rect(position.x + xDiv * 2f, position.y, xDiv, yDiv), "To:");
+        minValue = Mathf.Clamp(EditorGUI.IntField(
+            new Rect(position.x + xDiv + 30f, position.y, xDiv - 30f, yDiv), 
+            (int)minValue), attrib.min, maxValue);
+        EditorGUI.LabelField(
+            new Rect(position.x + xDiv * 2f, position.y, xDiv, yDiv), "To: ");
+        maxValue = Mathf.Clamp(
+            EditorGUI.IntField(
+                new Rect(position.x + xDiv * 2f + 24f, position.y, xDiv - 24f, yDiv), (int)maxValue), minValue, attrib.max);
+
+        min.intValue = Mathf.RoundToInt(minValue);
+        max.intValue = Mathf.RoundToInt(maxValue);
     }
 
     #endregion
