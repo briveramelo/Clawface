d84705a5f8861254cab928ae23ff53a24127d6bf
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2017-09-20 20:33:40 -0600

    Fix for non-existent respawn points

---
+++ Assets/Scripts/General/Singletons/RespawnPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPoint : Singleton<RespawnPoint> {
+
+    protected RespawnPoint() { }
+
+    void Start() {
+        if (GameObject.Find("RespawnPoint")) {
+            Destroy(gameObject);
+        }
+        else {
+            name = "RespawnPoint";
+        }
+    }
+}
