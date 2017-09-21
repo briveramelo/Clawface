91ed65b59bd1cda6d4afb6da8deb535126e894fe
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2017-09-20 20:09:04 -0600

    merging with ai, general, mod.. still some broken material references, still need to fixed hard code for respawn point

--- Assets/Scripts/Enemies/Bouncer/Bouncer.cs
+++ Assets/Scripts/Enemies/Bouncer/Bouncer.cs
@@ -42,7 +42,7 @@ public class Bouncer : MonoBehaviour, IStunnable, IDamageable, ISkinnable, ISpaw
         {
             ResetForRebirth();
         }
-        navAgent.enabled = true;
+        //navAgent.enabled = true;
     }
 
     void Awake()
