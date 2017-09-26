7283331a8276a5cb0f8d3e828b4bc65c5846c958
Chuan-Chin Lai <danny6430@hotmail.com>
2017-09-26 11:32:38 -0600

    added ObjectsEditor

---
+++ Assets/Scripts/Editor/ObjectsEditor/OET_add.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using OET_lib;
+
+namespace OET_add
+{
+	public class lib : MonoBehaviour
+    {
+		public static bool clickToAddEnabled = false;
+        public static bool previewDraw = false;
+
+        public static GameObject projectActiveSelection;
+		public static Vector3 mousePositionInScene;
+	
+		public static void sceneGUI ()
+        {
+			if (clickToAddEnabled && projectActiveSelection != null)
+            {
+				if(previewDraw)
+                {
+                    OET_lib.ToolLib.draft (projectActiveSelection, mousePositionInScene - projectActiveSelection.transform.position, Color.green);
+				}
+			}
+		}
+
+		public static void renderGUI(int vpos, GameObject get_projectActiveSelection)
+		{
+			projectActiveSelection = get_projectActiveSelection;
+			int width = Screen.width;
+			int height = Screen.height;
+			int btWidth = width < 160 ? width - 20 : 160;
+
+			GUIStyle styleInfoText = new GUIStyle(GUI.skin.box);
+			styleInfoText.wordWrap = true;
+			styleInfoText.normal.textColor = GUI.skin.label.normal.textColor;
+			styleInfoText.alignment = TextAnchor.MiddleLeft;
+
+			if (projectActiveSelection == null)
+            {
+                OET_lib.ToolLib.alertBox ("Prefab Placement", "Select a prefab in the project window to enable this tool.");
+			}
+            else
+            {
+				if (projectActiveSelection != null)
+                {
+					if (clickToAddEnabled)
+                    {
+						vpos += OET_lib.ToolLib.header ("<b>Click to Add</b>\nClick anywhere in the scene to add " + projectActiveSelection.name, vpos, true);
+					}
+                    else
+                    {
+						vpos += OET_lib.ToolLib.header ("<b>Click to Add</b>\nClick on the button below then click on the scene to add "  + projectActiveSelection.name, vpos, true);
+					}
+				}
+
+				Texture2D projectPreview = AssetPreview.GetAssetPreview (projectActiveSelection);
+				if(height > 310)
+                {
+					Color saveBg = GUI.backgroundColor;
+
+					if(clickToAddEnabled)
+                    {
+						GUI.backgroundColor = new Color(.5f, 0f, 0f, 1);
+					}
+
+					clickToAddEnabled = GUI.Toggle (new Rect (width / 2 - btWidth / 2, vpos, btWidth, 40), clickToAddEnabled, "Add to Scene", "button");
+					if(clickToAddEnabled) GUI.backgroundColor = saveBg;
+					vpos += 50;
+
+					if (projectPreview != null)
+                    {
+						GUI.Box (new Rect (width / 2 - 64, vpos, 128, 128), projectPreview);
+					}
+				}
+                else
+                {
+					int bth = height - vpos - 30;
+					if(bth > 128) bth = 128;
+
+					clickToAddEnabled = GUI.Toggle (new Rect ((width - bth - 30)/2 - btWidth / 2, vpos, btWidth, bth > 40 ? 40 : bth), clickToAddEnabled, "Add to Scene", "Button");
+					if (projectPreview != null)
+                    {
+						GUI.Box (new Rect (width -  bth - 10, vpos, bth, bth), projectPreview);
+					}
+				}
+			}
+		}
+
+		public static bool editorMouseEvent(Event e, GameObject projectActiveSelection)
+        {
+			previewDraw = false;
+			if (clickToAddEnabled && projectActiveSelection != null)
+            {
+				if(EditorWindow.mouseOverWindow != null && EditorWindow.mouseOverWindow.ToString () == " (UnityEditor.SceneView)")
+                {
+					Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+					RaycastHit hit;
+					if (Physics.Raycast(ray, out hit, 1000.0f)) 
+					{
+                        mousePositionInScene = hit.point;
+						previewDraw = true;
+						if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+						{
+							GameObject newAsset = Instantiate((GameObject)projectActiveSelection, mousePositionInScene, Quaternion.identity) as GameObject;
+							newAsset.name = projectActiveSelection.name;
+							newAsset.transform.rotation = projectActiveSelection.transform.rotation;
+							Undo.RegisterCreatedObjectUndo(newAsset, "Add object to scene");
+							Selection.activeObject = newAsset;
+						}
+					}
+				}
+			}
+			return clickToAddEnabled;
+		}
+	}
+}
+
---
+++ Assets/Scripts/Editor/ObjectsEditor/OET_duplicate.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using OET_lib;
+
+namespace OET_duplicate
+{
+	public class lib : MonoBehaviour
+    {
+		public static GameObject[] sceneSelection;
+		public static Vector2 scrollPosition = Vector2.zero;
+		public static bool countXenabled = true;
+		public static bool countYenabled = false;
+		public static bool countZenabled = false;
+		public static float spacing = 0;
+		public static Vector3 v3_;
+		public static GameObject sceneActiveSelection;
+
+		public static Vector3[] clones;
+
+		public static void sceneGUI ()
+        {
+			if (sceneActiveSelection != null && sceneSelection.Length == 1)
+            {
+				Vector3 Vsize = OET_lib.ToolLib.getAllBounds (sceneActiveSelection).size;
+				float size = Vsize.x > Vsize.y ? (Vsize.x > Vsize.z ? Vsize.x : Vsize.z) : (Vsize.y > Vsize.z ? Vsize.y : Vsize.z);
+				v3_ = Handles.PositionHandle(v3_, Quaternion.identity);
+				float d = Vector3.Distance(v3_,sceneActiveSelection.transform.position);
+
+				d = d + spacing * d;
+
+				int count = (int)(d / size); count = count < 1 ? 1 : count;
+
+				clones = new Vector3[(int)count];
+
+				for (float ptrX = 0f; ptrX < count; ptrX++)
+                {
+					Vector3 e = Vector3.Lerp (sceneActiveSelection.transform.position, v3_, ptrX / count);
+					clones[(int)ptrX] = v3_ - e;
+                    OET_lib.ToolLib.draft (sceneActiveSelection, v3_ - e, Color.yellow);
+				}
+
+				Handles.color = Color.green;
+				Handles.DrawDottedLine (v3_, sceneActiveSelection.transform.position, 5);
+			}
+		}
+
+		public static void renderGUI(int vpos, GameObject[] get_sceneSelection, GameObject get_sceneActiveSelection)
+		{
+			if(get_sceneActiveSelection && sceneActiveSelection != get_sceneActiveSelection) v3_ = get_sceneActiveSelection.transform.position + new Vector3(10,0,0);
+			sceneSelection = get_sceneSelection;
+
+			int width = Screen.width;
+			sceneActiveSelection = get_sceneActiveSelection;
+
+			if (get_sceneActiveSelection != null)
+            {
+				if(sceneSelection.Length != 1)
+                {
+                    OET_lib.ToolLib.alertBox ("Duplicate", "Select one single object in the hierarchy to enable this tool. Multiple objects are currently selected.");
+				}
+                else
+                {
+					vpos += OET_lib.ToolLib.header ("<b>Duplicate</b>\nDuplicate the selected object in the scene.", vpos, false);
+
+					GUI.Label (new Rect (10, vpos, 150, 20), "Spacing");
+					spacing = EditorGUI.Slider (new Rect (90, vpos, width - 100, 20), spacing, -1.0f, 2.0f);
+					vpos += 35;
+					float btWidth = width < 160 ? width - 20 : 160;
+					if (GUI.Button (new Rect (width / 2 - btWidth / 2, vpos, btWidth, 25), "Duplicate"))
+                    {
+
+						GameObject newClone = null;
+						GameObject p = PrefabUtility.GetPrefabParent(sceneActiveSelection) as GameObject;
+						var newSelection = new List<GameObject> ();
+
+						foreach(Vector3 thisClone in clones)
+                        {
+							if(p != null)
+                            {
+								newClone = PrefabUtility.InstantiatePrefab(p) as GameObject;
+								newClone.transform.position = thisClone + sceneActiveSelection.transform.position;
+								newClone.transform.rotation = sceneActiveSelection.transform.rotation;
+							}
+                            else
+                            {
+								newClone = Instantiate(sceneActiveSelection.gameObject, thisClone + sceneActiveSelection.transform.position, sceneActiveSelection.transform.rotation) as GameObject;
+								newClone.transform.localScale = sceneActiveSelection.transform.localScale;
+							}
+							newClone.transform.parent = sceneActiveSelection.transform.parent;
+							newClone.transform.localScale = sceneActiveSelection.transform.localScale;
+							Undo.RegisterCreatedObjectUndo (newClone, "Duplicate");
+							newSelection.Add (newClone);
+						}
+						Selection.objects = newSelection.ToArray ();
+					}
+				}
+			}
+            else
+            {
+                OET_lib.ToolLib.alertBox ("Duplicate", "Select an object in the hierarchy to enable this tool.");
+			}			
+		}
+	}
+}
+
