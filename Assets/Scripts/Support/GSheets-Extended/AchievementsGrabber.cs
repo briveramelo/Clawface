9db165bcb784a8ef45419605707b871227797988
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2017-10-04 01:41:11 -0600

    Working achievement manager and achievement grabber, achievement unit tester

--- Assets/Scripts/Support/GSheets-Extended/AchievementsGrabber.cs
+++ Assets/Scripts/Support/GSheets-Extended/AchievementsGrabber.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using SimpleJSON;
 
-public class AchievementsGrabber : Grabber<Achievement, AchievementScriptable, AchievementParser> {
+public class AchievementsGrabber : DataGrabber<Achievement, AchievementParser> {
 
-	
+
 }
 [System.Serializable]
-public class Achievement : GSheetData{
+public class Achievement : GSheetData {
     public string name;
     public string description;
     public int count;
-    public Achievement(string name, string description, int count) {
+    public int requiredCount;
+    public bool IsEarned { get { return count >= requiredCount; } }
+    public Achievement(string name, string description, int requiredCount) {
         this.name = name;
         this.description = description;
-        this.count = count;
+        this.requiredCount = requiredCount;
     }
 }
 [System.Serializable]
@@ -24,9 +26,4 @@ public class AchievementParser : GSheetsJSONParser<Achievement> {
     public override Achievement ParseValueElement(JSONArray array) {
         return new Achievement(array[0].Value, array[1].Value, array[2].AsInt);
     }
-}
-
-[System.Serializable]
-public class AchievementScriptable : GSheetScriptable<Achievement> {
-    
 }
