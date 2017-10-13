1658cb2df1c9fc872ed2e6fac3ae8597fe614696
Cornelia Xaos <cornelia.xaos@gmail.com>
2017-10-13 17:29:37 -0400

    Menu loop "complete".  Not 100% sure...
    There's some bugs.. some quality of life issues... and other things...
    
    Still.. you CAN pick your weapons now. :D

--- Assets/Scripts/UI/Menu/IMPL/WeaponSelectMenu.cs
+++ Assets/Scripts/UI/Menu/IMPL/WeaponSelectMenu.cs
@@ -4,155 +4,148 @@ using UnityEngine.UI;
 
 public class WeaponSelectMenu : Menu
 {
-    #region Accessors (Menu)
+	#region Accessors (Menu)
 
-    public override Button InitialSelection
-    {
-        get
-        {
-            return initialButton;
-        }
-    }
+	public override Button InitialSelection {
+		get {
+			return initialButton;
+		}
+	}
 
-    #endregion
+	#endregion
 
-    #region Fields (Unity Serialization)
+	#region Fields (Unity Serialization)
 
-    [SerializeField]
-    private Button initialButton;
+	[SerializeField]
+	private Button initialButton;
 
-    [SerializeField]
-    private Button startbutton;
+	[SerializeField]
+	private Button startButton;
 
-    [SerializeField]
-    private ButtonBundle[] weapons;
+	[SerializeField]
+	private ButtonBundle[] weapons;
 
-    #endregion
+	#endregion
 
-    #region Constructors (Public)
+	#region Constructors (Public)
 
-    public WeaponSelectMenu() : base(Strings.MenuStrings.WEAPON_SELECT) {}
+	public WeaponSelectMenu () : base (Strings.MenuStrings.WEAPON_SELECT)
+	{
+	}
 
-    #endregion
+	#endregion
 
-    #region Interface (Unity Lifecycle)
+	#region Interface (Unity Lifecycle)
 
-    private void Update()
-    {
-        PollWeaponSelect();
-    }
+	private void Update ()
+	{
+		PollWeaponSelect ();
+	}
 
-    #endregion
+	#endregion
 
-    #region Interface (Menu)
+	#region Interface (Menu)
 
-    protected override void DefaultHide(Transition transition, Effect[] effects)
-    {
-        Fade(transition, effects);
-    }
+	protected override void DefaultHide (Transition transition, Effect[] effects)
+	{
+		Fade (transition, effects);
+	}
 
-    protected override void DefaultShow(Transition transition, Effect[] effects)
-    {
-        Fade(transition, effects);
+	protected override void DefaultShow (Transition transition, Effect[] effects)
+	{
+		Fade (transition, effects);
+	}
 
-        // This menu should be displayed ALONE.
-        MenuManager.Instance.DoTransitionOthers(this, Transition.HIDE, new Effect[] { });
-    }
+	protected override void ShowStarted ()
+	{
+		base.ShowStarted ();
+		UpdateDisplay ();
+	}
 
-    protected override void ShowStarted()
-    {
-        UpdateDisplay();
-    }
+	#endregion
 
-    #endregion
+	#region Interface (Public)
 
-    #region Interface (Public)
+	public void BackAction ()
+	{
+		MenuManager.Instance.DoTransition (Strings.MenuStrings.LEVEL_SELECT,
+			Transition.SHOW, new Effect[] { Effect.EXCLUSIVE });
+	}
 
-    public void BackAction()
-    {
-        MenuManager.Instance.DoTransition(Strings.MenuStrings.LEVEL_SELECT,
-            Transition.SHOW, new Effect[] { Effect.EXCLUSIVE });
-    }
+	public void StartAction ()
+	{
+		// Acquire target level.
+		Menu menu = MenuManager.Instance.GetMenuByName (Strings.MenuStrings.LEVEL_SELECT);
+		LevelSelectMenu levelMenu = (LevelSelectMenu)menu;
+		string level = levelMenu.SelectedLevel;
 
-    public void StartAction()
-    {
-        // Acquire target level.
-        Menu menu = MenuManager.Instance.GetMenuByName(Strings.MenuStrings.LEVEL_SELECT);
-        LevelSelectMenu levelMenu = (LevelSelectMenu)menu;
-        string level = levelMenu.SelectedLevel;
+		// Acquire LoadMenu and set target.
+		menu = MenuManager.Instance.GetMenuByName (Strings.MenuStrings.LOAD);
+		LoadMenu loadMenu = (LoadMenu)menu;
+		loadMenu.TargetScene = level;
 
-        // Acquire LoadMenu and set target.
-        menu = MenuManager.Instance.GetMenuByName(Strings.MenuStrings.LOAD);
-        LoadMenu loadMenu = (LoadMenu)menu;
-        loadMenu.TargetScene = level;
+		// Make it happen.
+		MenuManager.Instance.DoTransition (loadMenu, Transition.SHOW,
+			new Effect[] { Effect.EXCLUSIVE });
+	}
 
-        // Make it happen.
-        MenuManager.Instance.DoTransition(loadMenu, Transition.SHOW,
-            new Effect[] { Effect.EXCLUSIVE });
-    }
+	#endregion
 
-    #endregion
+	#region Interface (Private)
 
-    #region Interface (Private)
+	private void PollWeaponSelect ()
+	{
+		ButtonMode leftMode = InputManager.Instance.QueryAction (Strings.Input.Actions.FIRE_LEFT);
+		ButtonMode rightMode = InputManager.Instance.QueryAction (Strings.Input.Actions.FIRE_RIGHT);
 
-    private void PollWeaponSelect()
-    {
-        ButtonMode leftMode = InputManager.Instance.QueryAction(Strings.Input.Actions.FIRE_LEFT);
-        ButtonMode rightMode = InputManager.Instance.QueryAction(Strings.Input.Actions.FIRE_RIGHT);
+		// Fast-fail
+		if (!(leftMode == ButtonMode.DOWN || rightMode == ButtonMode.DOWN))
+			return;
 
-        // Fast-fail
-        if (!(leftMode == ButtonMode.DOWN || rightMode == ButtonMode.DOWN))
-            return;
+		// Iterate through and identify selected "button"
+		GameObject selected = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+		foreach (ButtonBundle bundle in weapons) {
+			// Check to see if this weapon is selected
+			if (bundle.button.gameObject == selected) {
+				SetSelectedWeapon (bundle.type, leftMode == ButtonMode.DOWN);
+				UpdateDisplay ();
+			}
+		}
+	}
 
-        // Iterate through and identify selected "button"
-        GameObject selected = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
-        foreach (ButtonBundle bundle in weapons)
-        {
-            // Check to see if this weapon is selected
-            if (bundle.button.gameObject == selected)
-            {
-                SetSelectedWeapon(bundle.type, leftMode == ButtonMode.DOWN);
-                UpdateDisplay();
-            }
-        }
-    }
+	private void SetSelectedWeapon (ModType type, bool isLeftArm)
+	{
+		if (isLeftArm) {
+			ModManager.leftArmOnLoad = type;
+		} else {
+			ModManager.rightArmOnLoad = type;
+		}
+	}
 
-    private void SetSelectedWeapon(ModType type, bool isLeftArm)
-    {
-        if (isLeftArm)
-        {
-            ModManager.leftArmOnLoad = type;
-        } else
-        {
-            ModManager.rightArmOnLoad = type;
-        }
-    }
+	private void UpdateDisplay ()
+	{
+		foreach (ButtonBundle bundle in weapons) {
+			bundle.leftHand.SetActive (bundle.type == ModManager.leftArmOnLoad);
+			bundle.rightHand.SetActive (bundle.type == ModManager.rightArmOnLoad);
+		}
 
-    private void UpdateDisplay()
-    {
-        foreach (ButtonBundle bundle in weapons)
-        {
-            bundle.leftHand.SetActive(bundle.type == ModManager.leftArmOnLoad);
-            bundle.rightHand.SetActive(bundle.type == ModManager.rightArmOnLoad);
-        }
+		bool canStart = (ModManager.leftArmOnLoad != ModType.None &&
+		                ModManager.rightArmOnLoad != ModType.None);
+		startButton.interactable = canStart;
+	}
 
-        bool canStart = (ModManager.leftArmOnLoad != ModType.None &&
-            ModManager.rightArmOnLoad != ModType.None);
-        startbutton.interactable = canStart;
-    }
+	#endregion
 
-    #endregion
+	#region Types (Public)
 
-    #region Types (Public)
+	[Serializable]
+	public class ButtonBundle
+	{
+		public Button button;
+		public ModType type;
+		public GameObject leftHand;
+		public GameObject rightHand;
+	}
 
-    [Serializable]
-    public class ButtonBundle
-    {
-        public Button button;
-        public ModType type;
-        public GameObject leftHand;
-        public GameObject rightHand;
-    }
-
-    #endregion
+	#endregion
 }
