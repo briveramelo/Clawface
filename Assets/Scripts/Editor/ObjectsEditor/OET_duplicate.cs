d4b02d1ee52b768298bd79953dddcc3eaf93d0e8
Chuan-Chin Lai <danny6430@hotmail.com>
2017-10-19 12:38:19 -0600

    working on duplicate feature

--- Assets/Scripts/Editor/ObjectsEditor/OET_duplicate.cs
+++ Assets/Scripts/Editor/ObjectsEditor/OET_duplicate.cs
@@ -10,36 +10,47 @@ namespace OET_duplicate
     {
 		public static GameObject[] sceneSelection;
 		public static Vector2 scrollPosition = Vector2.zero;
-		public static bool countXenabled = true;
-		public static bool countYenabled = false;
-		public static bool countZenabled = false;
-		public static float spacing = 0;
+
 		public static Vector3 handle_v3;
 		public static GameObject sceneActiveSelection;
 
-		public static Vector3[] clones;
+        public static bool _x = true;
+        public static bool _z = true;
+
+        public static Vector3[] clones;
 
 		public static void sceneGUI ()
         {
 			if (sceneActiveSelection != null && sceneSelection.Length == 1)
             {
-				Vector3 Vsize = OET_lib.ToolLib.getAllBounds (sceneActiveSelection).size;
-				float size = Vsize.x > Vsize.y ? (Vsize.x > Vsize.z ? Vsize.x : Vsize.z) : (Vsize.y > Vsize.z ? Vsize.y : Vsize.z);
 				handle_v3 = Handles.PositionHandle(handle_v3, Quaternion.identity);
-				float d = Vector3.Distance(handle_v3,sceneActiveSelection.transform.position);
 
-				d = d + spacing * d;
+                handle_v3.y = sceneActiveSelection.transform.position.y;
+                handle_v3.z = sceneActiveSelection.transform.position.z;
+      
+				Vector3 d = handle_v3 - sceneActiveSelection.transform.position;
 
-				int count = (int)(d / size); count = count < 1 ? 1 : count;
+				int count = (int)(d.x / 5.0f);
+                clones = new Vector3[Mathf.Abs((int)count)];
 
-				clones = new Vector3[(int)count];
-
-				for (float ptrX = 0f; ptrX < count; ptrX++)
+                if (count > 0)
                 {
-					Vector3 e = Vector3.Lerp (sceneActiveSelection.transform.position, handle_v3, ptrX / count);
-					clones[(int)ptrX] = handle_v3 - e;
-                    OET_lib.ToolLib.draft (sceneActiveSelection, handle_v3 - e, Color.yellow);
-				}
+                    for (int i = 1; i < count; i++)
+                    {
+                        Vector3 tmp = new Vector3(5 * i, 0, 0);
+                        clones[i] = tmp;
+                        OET_lib.ToolLib.draft(sceneActiveSelection, tmp, Color.yellow);
+                    }
+                }
+                else
+                {
+                    for (int i = count; i < 0; i++)
+                    {
+                        Vector3 tmp = new Vector3(5 * i, 0, 0);
+                        clones[Mathf.Abs(i) - 1] = tmp;
+                        OET_lib.ToolLib.draft(sceneActiveSelection, tmp, Color.yellow);
+                    }
+                }
 
 				Handles.color = Color.green;
 				Handles.DrawDottedLine (handle_v3, sceneActiveSelection.transform.position, 5);
@@ -64,9 +75,6 @@ namespace OET_duplicate
                 {
 					vpos += OET_lib.ToolLib.header ("<b>Duplicate</b>\nDuplicate the selected object in the scene.", vpos, false);
 
-					GUI.Label (new Rect (10, vpos, 150, 20), "Spacing");
-					spacing = EditorGUI.Slider (new Rect (90, vpos, width - 100, 20), spacing, -1.0f, 2.0f);
-					vpos += 35;
 					float btWidth = width < 160 ? width - 20 : 160;
 					if (GUI.Button (new Rect (width / 2 - btWidth / 2, vpos, btWidth, 25), "Duplicate"))
                     {
@@ -100,8 +108,31 @@ namespace OET_duplicate
             else
             {
                 OET_lib.ToolLib.alertBox ("Duplicate", "Select an object in the hierarchy to enable this tool.");
-			}			
-		}
+			}
+
+            GUI.Label(new Rect(10, vpos, 150, 20), "x");
+            _x = GUI.Toggle(new Rect(160, vpos, 50, 20), _x, "");
+            vpos += 20;
+
+            GUI.Label(new Rect(10, vpos, 150, 20), "z");
+            _z = GUI.Toggle(new Rect(160, vpos, 50, 20), _z, "");
+            vpos += 20;
+
+            _UI();
+        }
+
+
+        static void _UI()
+        {
+            if(_x)
+            {
+                _z = false;
+            }
+            else if(_z)
+            {
+                _x = false;
+            }
+        }
 	}
 }
 
