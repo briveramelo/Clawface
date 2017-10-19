3ec064f78ea0e3a941a497c1b623c7b6e04c9d11
garink <garinrk@gmail.com>
2017-10-19 12:16:54 -0600

    Added GoreManager to ServiceWrangler | PSManager will use the new GoreManager | Created GoreManager gameobject prefab for the object pool

--- Assets/Scripts/General/Singletons/GoreManager.cs
+++ Assets/Scripts/General/Singletons/GoreManager.cs
@@ -2,6 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoreManager : MonoBehaviour {
+public class GoreManager : Singleton<GoreManager> {
+
+    //List<Vector3> splatsInWorld = new List<Vector3>
+
+    public void SpawnSplat(Vector3 worldPos) {
+
+        RaycastHit hit;
+
+        if (Physics.SphereCast(worldPos, 1f, transform.forward, out hit, 1f))
+        {
+
+            GameObject hitSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            hitSphere.transform.position = worldPos;
+            GameObject decal = ObjectPool.Instance.GetObject(PoolObjectType.VFXBloodDecal);
+            Material bloodMat = decal.GetComponent<MeshRenderer>().material;
+
+            if (bloodMat)
+            {
+                Texture bloodMask = bloodMat.GetTexture("_BloodMask");
+
+#if UNITY_EDITOR
+                Debug.Log("W:\t" + bloodMask.width + " H:\t" + bloodMask.height);
+#endif
+            }
+        }
+    }
     
 }
