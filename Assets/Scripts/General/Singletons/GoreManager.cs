69c045bb1559a5804d673ec7b0040ace7c608717
garink <garinrk@gmail.com>
2017-10-24 10:25:11 -0600

    Working on goremanager stuff

--- Assets/Scripts/General/Singletons/GoreManager.cs
+++ Assets/Scripts/General/Singletons/GoreManager.cs
@@ -2,29 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//world position
+//normal at world position
+//previous splat texture (via splattable)
+
 public class GoreManager : Singleton<GoreManager> {
-
-    //List<Vector3> splatsInWorld = new List<Vector3>
-
+    
     public void SpawnSplat(Vector3 worldPos) {
 
         RaycastHit hit;
 
         if (Physics.SphereCast(worldPos, 1f, transform.forward, out hit, 1f))
         {
-
-            GameObject hitSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            hitSphere.transform.position = worldPos;
-            GameObject decal = ObjectPool.Instance.GetObject(PoolObjectType.VFXBloodDecal);
-            Material bloodMat = decal.GetComponent<MeshRenderer>().material;
-
-            if (bloodMat)
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.tag == Strings.Tags.FLOOR || hitObject.tag == Strings.Tags.WALL)
             {
-                Texture bloodMask = bloodMat.GetTexture("_BloodMask");
+                //Splattable s = hitObject.GetComponent<Splattable>();
+                Splattable splattable = null;
+                
+                GameObject hitSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                hitSphere.transform.position = worldPos;
+                GameObject decal = ObjectPool.Instance.GetObject(PoolObjectType.VFXBloodDecal);
+                Material bloodMat = decal.GetComponent<MeshRenderer>().material;
+
+                if (bloodMat)
+                {
+                    Texture bloodMask = bloodMat.GetTexture("_BloodMask");
 
 #if UNITY_EDITOR
-                Debug.Log("W:\t" + bloodMask.width + " H:\t" + bloodMask.height);
+                    Debug.Log(hitObject.name + " W:" + bloodMask.width + " H:" + bloodMask.height);
 #endif
+                }
             }
         }
     }
