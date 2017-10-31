43a3ec405cba33d48fa68baf10f655f7cfe03198
Cornelia Xaos <cornelia.xaos@gmail.com>
2017-10-31 14:07:13 -0600

    Updated GoreManager debugging options.

--- Assets/Scripts/General/Singletons/GoreManager.cs
+++ Assets/Scripts/General/Singletons/GoreManager.cs
@@ -17,26 +17,60 @@ public class GoreManager : Singleton<GoreManager> {
     [SerializeField]
     private float sphereRadius = 1F;
 
+    #if UNITY_EDITOR
+
+    [SerializeField]
+    private bool debugSplats = false;
+
+    #endif
+
     #endregion
 
+    #region Fields (Private)
+
     private int mask;
+    #if UNITY_EDITOR
+
+    GameObject debugSpheres;
+
+    #endif
+
+    #endregion
 
     protected override void Awake()
     {
         base.Awake();
         mask = 1 << 11; //GROUND LAYER
         uvSpaceCamera.aspect = 1;
+
+        #if UNITY_EDITOR
+        
+        debugSpheres = new GameObject("Debug Spheres");
+        debugSpheres.transform.SetParent(gameObject.transform);
+
+        #endif
     }
+
+    #if UNITY_EDITOR
+    private void OnLevelWasLoaded(int level)
+    {
+        Destroy(debugSpheres);
+        debugSpheres = new GameObject("Debug Spheres");
+        debugSpheres.transform.SetParent(gameObject.transform);
+    }
+    #endif
 
     public void SpawnSplat(Vector3 worldPos) {
 
         Collider[] collided = Physics.OverlapSphere(worldPos, sphereRadius, mask);
 
         #if UNITY_EDITOR
-        if (false && collided.Length != 0)
+        if (debugSplats && collided.Length != 0)
         {
             GameObject hitSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            hitSphere.transform.SetParent(debugSpheres.transform);
             hitSphere.transform.position = worldPos;
+            hitSphere.transform.localScale = new Vector3(sphereRadius, sphereRadius, sphereRadius);
             hitSphere.GetComponent<Collider>().enabled = false;
         }
         #endif
