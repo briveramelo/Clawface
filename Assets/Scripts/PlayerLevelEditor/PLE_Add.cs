11298bf32e04f390c2f4fd3fa1a7c58992aafa8c
Chuan-Chin Lai <danny6430@hotmail.com>
2017-11-16 13:15:57 -0700

    update update PLE_add DB feature

--- Assets/Scripts/PlayerLevelEditor/PLE_Add.cs
+++ Assets/Scripts/PlayerLevelEditor/PLE_Add.cs
@@ -5,9 +5,14 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using PLE_ToolKit;
 
+using Turing.LevelEditor;
+
+
+
+
 public class PLE_Add : PLE_IFunction
 {
-    GameObject _prefab;
+    public static GameObject _prefab;
 
     Button  Btn_Add;
     Button  Btn_Delete;
@@ -20,6 +25,13 @@ public class PLE_Add : PLE_IFunction
 
     private bool clickToAddEnabled = false;
     private bool clickToDeleteEnabled = false;
+
+
+
+    GameObject DB_List;
+
+
+    List<LAIButton> BTS = new List<LAIButton>();
 
     public PLE_Add(PLE_FunctionController Controller) : base(Controller)
 	{
@@ -47,8 +59,43 @@ public class PLE_Add : PLE_IFunction
         Btn_Delete.onClick.AddListener(ACT_Delete);
         Btn_Delete.image.color = Color.white;
 
-
         _prefab = Resources.Load("LevelEditorObjects/CommonArea/test") as GameObject;
+
+
+        {
+            DB_List = PLE_ToolKit.UITool.FindUIGameObject("DB_List");
+
+            GameObject _ItemExample = PLE_ToolKit.UITool.FindUIGameObject("ItemExample");
+
+            GameObject[] _DBObject;
+            _DBObject = Resources.LoadAll<GameObject>("LevelEditorObjects") as GameObject[];
+
+            foreach(GameObject _object in _DBObject)
+            {
+                GameObject _item = GameObject.Instantiate(_ItemExample);
+
+                _item.SetActive(true);
+                _item.name = "Item";
+                _item.GetComponentInChildren<Text>().text = _object.name;
+
+                LAIButton _BT = new LAIButton(PLE_ToolKit.UnityTool.FindChildGameObject(_item, "Button").GetComponent<Button>(), _object);
+
+#if UNITY_EDITOR
+                Texture2D   _texture = UnityEditor.AssetPreview.GetAssetPreview(_object);
+#else   
+                Debug.LogWarning("Need to get a proper thumbnail for level editor prop assets!");
+                Texture2D _texture = Texture2D.whiteTexture;
+#endif
+
+                Sprite _sprite = Sprite.Create(_texture, new Rect(0, 0, _texture.width, _texture.height), new Vector2(0.5f, 0.5f));
+                _item.GetComponentInChildren<Image>().sprite = _sprite;
+
+
+                _item.transform.SetParent(DB_List.transform);
+            }
+        }
+
+
     }
 
 
@@ -103,3 +150,33 @@ public class PLE_Add : PLE_IFunction
     }
 
 }
+
+
+public class LAIButton
+{
+    Button Button;
+    UnityAction Action;
+    GameObject DBObject;
+
+    public LAIButton(Button i_Button, GameObject i_Object)
+    {
+        Action = () => OnClick(Button);
+
+        Button = i_Button;
+        Button.onClick.AddListener(Action);
+
+        DBObject = i_Object;
+    }
+
+    ~LAIButton()
+    {
+        Button.onClick.RemoveListener(Action);
+    }
+
+
+    public void OnClick(Button thisBtn)
+    {
+        PLE_Add._prefab = DBObject;
+    }
+}
+
