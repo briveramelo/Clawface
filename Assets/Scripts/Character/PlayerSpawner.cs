09246ffa20da726d296f8d4fa447792170e5df3f
garink <garinrk@gmail.com>
2017-11-21 10:03:56 -0700

    Progress

--- Assets/Scripts/Character/PlayerSpawner.cs
+++ Assets/Scripts/Character/PlayerSpawner.cs
@@ -16,15 +16,20 @@ public class PlayerSpawner : MonoBehaviour
 {
     #region Private Fields
 
-    const string PLAYER_PREFAB_PATH = "/Prefabs/Player/";
-    const string RESOURCES_FOLDER_PATH = "/Resources/Player/";
-    const string PLAYER_GROUP_NAME = "Keira_GroupV";
-    const string PREFAB_EXT = ".prefab";
+    private const string PLAYER_PREFAB_PATH = "/Prefabs/Player/";
+    private const string PLAYER_RESOURCES_FOLDER_PATH = "/Resources/Player/";
+    private const string PLAYER_GROUP_NAME = "Keira_GroupV";
+    private const string PREFAB_EXT = ".prefab";
+
+    private const string PLAYER_UI_PREFAB_PATH = "/Prefabs/UI/";
+    private const string PLAYER_UI_RESOUCES_FOLDER_PATH = "Resources/PlayerUI/";
+    private const string PLAYER_UI_NAME = "PlayerHUDV";
+
 
     // Comes from the player prefab
-    static Vector3 SPAWN_OFFSET = new Vector3 (-14f, 20f, -7f);
+    private static Vector3 SPAWN_OFFSET = new Vector3 (-14f, 20f, -7f);
 
-    GameObject player;
+    private GameObject player;
 
     new Camera camera;
 
@@ -51,13 +56,23 @@ public class PlayerSpawner : MonoBehaviour
     {
         // Get the path of the newest prefab
         string playerPrefabPath = GetNewestPlayerPrefabPath();
+        string playerUIPrefabPath = GetNewestPlayerUIPrefabPath();
         
         // Load the prefab
-        GameObject playerPrefab = Resources.Load<GameObject>(playerPrefabPath);        
+        GameObject playerPrefab = Resources.Load<GameObject>(playerPrefabPath);
         if (playerPrefab == null)
-            throw new NullReferenceException (
-                string.Format ("Failed to load prefab at \"{0}\"!", 
-                playerPrefabPath));
+        {
+            throw new NullReferenceException(
+                string.Format("Failed to load prefab at \"{0}\"!",
+                    playerPrefabPath));
+        }
+        GameObject playerUIPrefab = Resources.Load<GameObject>(playerUIPrefabPath);
+        if (playerUIPrefab == null)
+        {
+            throw new NullReferenceException(
+                string.Format("Failed to load prefab at \"{0}\"!",
+                    playerUIPrefabPath));
+        }
 
         // Instantiate the prefab and bring it to spawner location
         if (Application.isPlaying)
@@ -80,9 +95,39 @@ public class PlayerSpawner : MonoBehaviour
 
     string GetNewestPlayerUIPrefabPath()
     {
-        //VersionedPlayerPrefab
+        VersionedPlayerUIPrefab[] allPrefabs =
+            Resources.LoadAll<VersionedPlayerUIPrefab>("PlayerUI/");
+        List<GameObject> results = new List<GameObject>();
 
-        return "";
+        float highestVersion = Mathf.NegativeInfinity;
+        string highestVersionName = "";
+        foreach (VersionedPlayerUIPrefab prefab in allPrefabs)
+        {
+            string name = prefab.gameObject.name;
+            string versionText = name.Remove(0,
+                PLAYER_GROUP_NAME.Length);
+
+            try
+            {
+                // Attempt to parse to float, and check if it is newest
+                float version = float.Parse(versionText);
+                if (version > highestVersion)
+                {
+                    highestVersion = version;
+                    highestVersionName = name;
+                }
+            }
+
+            catch (FormatException)
+            {
+                Debug.LogError(string.Format(
+                    "Failed to parse version number at {0}!", name));
+                continue;
+            }
+        }
+
+        return string.Format("{0}{1}", "Player/", highestVersionName);
+        
     }
 
     /// <summary>
