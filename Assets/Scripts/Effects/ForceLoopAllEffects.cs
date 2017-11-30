428e2c7864bf615b856fd39a5ee58e579adb3b6d
Aaron Desin <aaron@aarondesin.com>
2017-11-30 11:29:52 -0700

    VFX test scene

---
+++ Assets/Scripts/Effects/ForceLoopAllEffects.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Turing.VFX;
+
+public class ForceLoopAllEffects : MonoBehaviour
+{
+
+    [SerializeField]
+    float interval = 2.0f;
+
+    VFXOneOff[] effects;
+
+    private void Start()
+    {
+        effects = GetComponentsInChildren<VFXOneOff>();
+        StartCoroutine(LoopCoroutine());
+    }
+
+    IEnumerator LoopCoroutine()
+    {
+        while (true)
+        {
+
+            foreach (VFXOneOff vfx in effects) vfx.Play();
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
