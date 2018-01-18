1d08b48a82d75d7c86d9468ed40d49eea69b3940
Chuan-Chin Lai <danny6430@hotmail.com>
2018-01-18 12:42:32 -0700

    working on GridLevelSystem

---
+++ Assets/Scripts/PlayerLevelEditor/PlayerLevelEditorGrid.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLevelEditorGrid : MonoBehaviour
+{
+
+    GameObject OnClickObject = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        var hits = Physics.BoxCastAll(Vector3.zero, new Vector3(1, 1, 1), Vector3.right, Quaternion.identity, 10000);
+        Debug.Log("Hits length: " + hits.Length);
+
+        foreach (var item in hits)
+        {
+            Debug.Log(item.transform.name);
+            Debug.Log(item.transform.position);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000.0f))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Click: ");
+                Debug.Log(hit.transform.position);
+
+                OnClickObject = hit.transform.gameObject;
+            }
+
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                Debug.Log("Release: ");
+                Debug.Log(hit.transform.position);
+
+                float xMax = Mathf.Max(OnClickObject.transform.position.x, hit.transform.position.x);
+                float xMin = Mathf.Min(OnClickObject.transform.position.x, hit.transform.position.x);
+
+                float zMax = Mathf.Max(OnClickObject.transform.position.z, hit.transform.position.z);
+                float zMin = Mathf.Min(OnClickObject.transform.position.z, hit.transform.position.z);
+
+
+                var HitsInX = Physics.BoxCastAll(new Vector3(xMin, 0, zMin), new Vector3(1, 1, 1), Vector3.right, Quaternion.identity, xMax - xMin);
+
+                {
+                    foreach (var itemX in HitsInX)
+                    {
+                        Debug.Log(itemX.transform.position);
+                        itemX.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
+
+                        var HitsInZ = Physics.BoxCastAll(itemX.transform.position, new Vector3(1, 1, 1), Vector3.forward, Quaternion.identity, zMax - zMin);
+
+                        foreach (var itemZ in HitsInZ)
+                        {
+                            itemZ.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
