3f637aed353b26090272a35a7117d4846785b5b3
MegaSonic <adamkay90@gmail.com>
2018-01-23 12:43:04 -0700

    Working on deathcam spinning

---
+++ Assets/Scripts/Camera/DeathcamSpin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class DeathcamSpin : MonoBehaviour {
+
+    #region Serialized
+    [SerializeField] private float spinSpeed;
+    [SerializeField] private CinemachineVirtualCamera cam;
+    #endregion
+
+
+    #region Unity Lifecycle
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+#endregion
+}
