f6d79faff23647127d748b4fe2134c381ae0425f
Chuan-Chin Lai <danny6430@hotmail.com>
2018-01-23 17:07:43 -0700

    update Grid System

--- Assets/Scripts/Helpers/Strings.cs
+++ Assets/Scripts/Helpers/Strings.cs
@@ -189,7 +189,7 @@
         public const string LEVEL_OBJECT = "LEVEL";
 
         //objects
-        public const string BASIC_LVL_BLOCK = "Env/Block1";
+        public const string BASIC_LVL_BLOCK = "Env/MockBlock";
     }
 
     
