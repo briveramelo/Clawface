5045dbc849c2157dda0e83ded735871e99deeb0f
Chuan-Chin Lai <danny6430@hotmail.com>
2018-02-01 16:57:02 -0700

    working on wave and dynamic level system

--- Assets/Scripts/LevelEditor/WaveSystem.cs
+++ Assets/Scripts/LevelEditor/WaveSystem.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveSystem : MonoBehaviour {
+public class WaveSystem : MonoBehaviour
+{
+    public static int wave = 0;
 
     public void DoTheThing()
     {
-
+        Debug.Log(wave);
+        wave = wave < 2 ? ++wave : 0;
     }
 }
