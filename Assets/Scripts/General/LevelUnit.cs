fc287b14583ef488519dfd8f9e86906e98ed4dd4
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2018-02-06 12:46:53 -0700

    removed printing

--- Assets/Scripts/General/LevelUnit.cs
+++ Assets/Scripts/General/LevelUnit.cs
@@ -275,7 +275,6 @@ public class LevelUnit : RoutineRunner {
         Color newColor = startColor + (targetColor - startColor) * progress;
         materialPropertyBlock.SetColor(AlbedoTint, newColor);
         meshRenderer.SetPropertyBlock(materialPropertyBlock);
-        print("changing color");
     }
     #endregion
 
