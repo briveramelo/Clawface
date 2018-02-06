1bf990e5beb95561fd67beb44588f1af6d3075aa
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2018-02-06 13:47:00 -0700

    Auto reparening on disable for object pool items

---
+++ Assets/Scripts/Helpers/ObjectPoolItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolItem : MonoBehaviour {
+
+    Transform myParent;
+    public void SetParent(Transform newParent) {
+        myParent = newParent;
+    }
+
+    void OnDisable() {
+        transform.SetParent(myParent);
+    }
+}
