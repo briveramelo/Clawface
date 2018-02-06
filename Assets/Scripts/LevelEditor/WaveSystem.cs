ae46f9e45e30041214cdfd4f2b0f68f6533644ab
Chuan-Chin Lai <danny6430@hotmail.com>
2018-02-06 11:33:19 -0700

    update Level Editor, wave system

--- Assets/Scripts/LevelEditor/WaveSystem.cs
+++ Assets/Scripts/LevelEditor/WaveSystem.cs
@@ -4,11 +4,48 @@ using UnityEngine;
 
 public class WaveSystem : MonoBehaviour
 {
-    public static int wave = 0;
+    public static int maxWave = 3;
+    public static int currentWave = 0;
+
+    private void Start()
+    {
+//        for (int i = 0; i < maxWave; i++)
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            UpdateLevelUnitState();
+        }
+    }
 
     public void DoTheThing()
     {
-        Debug.Log(wave);
-        wave = wave < 2 ? ++wave : 0;
+        Debug.Log(currentWave++);
+
+        if (currentWave >= 3)
+            currentWave = 0;
+    }
+
+    public void UpdateLevelUnitState()
+    {
+        if (EventSystem.Instance)
+        {
+            EventSystem.Instance.TriggerEvent(Strings.Events.PLE_UPDATE_LEVELSTATE);
+
+
+            string wave = "PLE_TEST_WAVE_" + currentWave.ToString();
+            EventSystem.Instance.TriggerEvent(wave);
+        }
+    }
+
+
+    public void AddWave()
+    {
+        if (EventSystem.Instance)
+        {
+            EventSystem.Instance.TriggerEvent(Strings.Events.PLE_ADD_WAVE);
+        }
     }
 }
