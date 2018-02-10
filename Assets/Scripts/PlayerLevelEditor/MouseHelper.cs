94525bb52de1018e04a1119d620609a066c6a6e3
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2018-02-09 20:45:17 -0700

    prop placement works properly

--- Assets/Scripts/PlayerLevelEditor/MouseHelper.cs
+++ Assets/Scripts/PlayerLevelEditor/MouseHelper.cs
@@ -12,24 +12,40 @@ public class MouseHelper : MonoBehaviour {
     public static Vector3 currentMouseWorldPosition;
     public static RaycastHit raycastHit;
     public static bool hitItem;
+    public static bool hitTile;
     #endregion
 
     #region Private Fields
 
     private Ray r;
-
+    private int layerMask;
+    private RaycastHit raycastHitTile;
     #endregion
 
     #region Unity Lifecycle
 
-    private void Update()
-    {
-        r = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    private void Start() {
+        layerMask = LayerMask.GetMask(Strings.Layers.GROUND);
+    }
+    private void Update() {
+        r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
         hitItem = Physics.Raycast(r, out raycastHit, 1000.0f);
         if (hitItem) {
             currentHoveredObject = raycastHit.transform.gameObject;
-            currentBlockUnit = currentHoveredObject.GetComponent<PLEBlockUnit>();
             currentMouseWorldPosition = raycastHit.transform.position;
+        }
+        else {
+            currentHoveredObject = null;
+        }
+
+        hitTile = Physics.Raycast(r, out raycastHitTile, 1000.0f, layerMask);
+        if (hitTile) {
+            currentBlockUnit = raycastHitTile.transform.gameObject.GetComponent<PLEBlockUnit>();
+        }
+        else {
+            currentBlockUnit = null;
         }
     }
 
