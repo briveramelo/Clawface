2a101181349590023c3adbd46e3dd5b2f966531f
garink <garinrk@gmail.com>
2018-02-22 13:01:06 -0700

    PLESpawnManager work

---
+++ Assets/Scripts/PlayerLevelEditor/PLESpawnManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLESpawnManager : Singleton<PLESpawnManager> {
+
+    List<List<PLESpawn>> spawners = new List<List<PLESpawn>>();
+
+    public void Begin()
+    {
+
+    }
+
+    public void RegisterSpawner(int i_wave, PLESpawn i_spawn)
+    {
+
+    }
+}
