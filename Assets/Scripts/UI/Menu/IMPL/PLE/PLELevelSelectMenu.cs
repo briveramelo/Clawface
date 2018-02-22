a29756285a92b32147c017803fc2541f27fff64d
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2018-02-22 15:25:25 -0700

    moved ple menu items into a folder for cleanliness

---
+++ Assets/Scripts/UI/Menu/IMPL/PLE/PLELevelSelectMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PLELevelSelectMenu : Menu {
+    public PLELevelSelectMenu() : base (Strings.MenuStrings.LEVEL_SELECT)
+	{
+    }
+    public override Button InitialSelection {
+        get {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    protected override void ShowComplete() {
+        base.ShowComplete();
+    }
+
+    protected override void ShowStarted() {
+        base.ShowStarted();
+    }
+
+    protected override void HideComplete() {
+        base.HideComplete();
+    }
+
+    protected override void HideStarted() {
+        base.HideStarted();
+    }    
+
+    protected override void DefaultHide(Transition transition, Effect[] effects) {
+        
+    }
+
+    protected override void DefaultShow(Transition transition, Effect[] effects) {
+        
+    }
+}
