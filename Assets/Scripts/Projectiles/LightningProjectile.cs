6bda5fa87ec9100eb17bbe7245c5f92c2c03a7dc
MegaSonic <adamkay90@gmail.com>
2018-03-05 17:05:45 -0700

    Fixed small lightning gun bug where lightning projectiles could get stuck in the air and not know what to do

--- Assets/Scripts/Projectiles/LightningProjectile.cs
+++ Assets/Scripts/Projectiles/LightningProjectile.cs
@@ -51,6 +51,7 @@ public class LightningProjectile : MonoBehaviour {
             {
                 if (!CheckForTargets())
                 {
+                    ResetToDefaults();
                     return;
                 }
             }
